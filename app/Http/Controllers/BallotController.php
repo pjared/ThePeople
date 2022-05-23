e3b76d0e9ab399643dc4625994e464b12f3de9ac
pjared <pjared870@gmail.com>
2022-05-22 20:13:59 -0600

    Add getBallot implementation

--- app/Http/Controllers/BallotController.php
+++ app/Http/Controllers/BallotController.php
@@ -2,12 +2,44 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Candidate;
 use Illuminate\Http\Request;
 
 class BallotController extends Controller
 {
-    public function getBallot() {
+    /**
+     * The get ballot method will retrieve the candidates for a specific office and location.
+     * 
+     * Retrieves the following:
+     *  - 
+     *  -
+     * Returns view: ballot/index
+     */
+    public function getBallot(Request $request) {
+        //Validate the incoming data
+        // dd($request);
+        $validated = $request->validate([
+            'location' => 'required',
+            'location_type' => 'required',
+            'office' => 'required'
+        ]);
+
+        //Get the location that the user input
+        $location_type = $request->location_type;
+        $location = $request->location;
+        $office = $request->office;
+
+        switch ($location_type) {
+            case 'state':
+                break;
+            case 'city':
+                break; 
+            case 'zip':
+                break;
+        }
         
-        return view('ballot.index');
+        $candidates = Candidate::all();
+        return view('ballot.index')
+                    ->with('candidates', $candidates);
     }
 }
