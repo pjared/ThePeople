2bbb4f147f6402d787d92461c998bef1afe476bf
pjared <pjared870@gmail.com>
2022-05-25 13:40:20 -0600

    turns candidate/politician law into pivots

--- app/Models/CandidateLaw.php
+++ app/Models/CandidateLaw.php
@@ -3,9 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Pivot;
 
-class CandidateLaw extends pivot
+class CandidateLaw extends Pivot
 {
     use HasFactory;
 
--- app/Models/PoliticianOfficePositions.php
+++ app/Models/PoliticianOfficePositions.php
@@ -3,9 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Pivot;
 
-class PoliticanOfficePositions extends Model
+class PoliticanOfficePositions extends Pivot
 {
     public $timestamps = false;
 
