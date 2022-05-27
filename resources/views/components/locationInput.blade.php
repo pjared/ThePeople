3da6e583eee77830eab9f01810cfe1356cd0b982
pjared <pjared870@gmail.com>
2022-05-27 15:44:31 -0600

    center location

--- resources/views/components/locationInput.blade.php
+++ resources/views/components/locationInput.blade.php
@@ -2,7 +2,7 @@
     <div class="mt-1">
         <div class="form-group row">
             <label for="stateInput" class="col-sm-2 col-form-label locationInput">State: </label>
-            <div class="col-sm-3 no-padding">
+            <div class="col-sm-3 no-padding" style="width:40%">
                 <input class="form-control" id="stateInput" type="text" placeholder="">
             </div>
         </div>
@@ -10,7 +10,7 @@
     <div class="mt-1">
         <div class="form-group row">
             <label for="cityInput" class="col-sm-2 col-form-label locationInput">City:</label>
-            <div class="col-sm-3 no-padding">
+            <div class="col-sm-3 no-padding" style="width:50%">
                 <input class="form-control" id="cityInput" type="text" placeholder="">
             </div>
         </div>                        
@@ -18,7 +18,7 @@
     <div class="mt-1">
         <div class="form-group row">
             <label for="zipInput" class="col-sm-2 col-form-label locationInput">Zip:</label>
-            <div class="col-sm-2 no-padding">
+            <div class="col-sm-2 no-padding" style="width:30%">
                 <input class="form-control" id="zipInput" type="text" placeholder="">
             </div>
         </div>                        
