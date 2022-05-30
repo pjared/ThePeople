ba759f6693add3eb171033ff0f4bd55012bfff13
pjared <pjared870@gmail.com>
2022-05-30 14:11:10 -0600

    Adds routes for login/out

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\BallotController;
 use App\Http\Controllers\CandidateController;
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\PoliticianController;
 use App\Http\Controllers\SearchController;
 use Illuminate\Support\Facades\Route;
@@ -17,14 +18,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/* -----HOME------ */
 Route::get('/', function () {
     return view('home');
 });
 
+/* -----AUTHENTICATION------ */
+Route::get('/login-page', function() {
+    return view('login');
+});
+
+Route::post('/login', [LoginController::class, 'authenticate']);
+
+Route::get('/logout', [LoginController::class, 'logout']);
+
+/* -----CANDIDATE------ */
 Route::get('/profile/candidate/{id}', [CandidateController::class, 'getCandidateView']);
 
+/* -----BALLOT------ */
 Route::get('/ballot', [BallotController::class, 'getBallot']);
 
+/* -----ASYNC AUTOCOMPLETE CALLS------ */
 Route::group(['namespace' => 'autocomplete', 'prefix' => 'autocomplete'], function() {
     Route::get('/state', [SearchController::class, 'autocompleteState'])->name('state');
 });
