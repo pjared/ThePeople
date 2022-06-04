1d4793752ab947716fe9f55587f468a543410f4c
pjared <pjared870@gmail.com>
2022-06-04 12:13:12 -0600

    renames home to welcome

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ use Illuminate\Support\Facades\Route;
 
 /* -----HOME------ */
 Route::get('/', function () {
-    return view('home');
+    return view('welcome');
 });
 
 /* -----AUTHENTICATION------ */
