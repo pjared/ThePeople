1141e7715b2dc6ac20466c95aac636ad2a8db81d
pjared <pjared870@gmail.com>
2022-06-10 08:15:17 -0600

    Adds ability to get a user profile

---
+++ resources/views/profile/show.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-2"></div>
+        <div class="col-8">
+            <div class="text-center">
+                {{-- TODO: Implement some basic information for the user to fill out here --}}
+                <a href="/candidate-register">Are you a candidate? Click here to make your profile</a>
+            </div>
+        </div>
+        <div class="col-2"></div>
+    </div>
+</div>
+
+@endsection
