e8454d342fb5277738cccd5ef3f2a593ecb76de5
pjared <pjared870@gmail.com>
2022-06-19 15:26:43 -0600

    Adds voting date when hovering a ballot

--- resources/views/livewire/ballot-list.blade.php
+++ resources/views/livewire/ballot-list.blade.php
@@ -1,10 +1,13 @@
 <div class="flex grow flex-row flex-wrap gap-8 justify-center mt-12 p-6 w-11/12">
     @foreach ($ballots as $ballot)
         {{-- TODO: Make this entire thing clickable --}}
-        <form action="/ballot/{{$ballot->id}}" method="GET" class="min-w-fit w-1/4 hover:scale-110">
-            <button type="submit" class="card w-full h-full">
+        <form action="/ballot/{{$ballot->id}}" method="GET" class="min-w-fit w-1/4 hover:scale-110" x-data="{ show: false }" @mouseleave="show = false">
+            <button @mouseover="show = true" type="submit" class="card w-full h-full">
                 {{-- {{$ballot->location->name}} {{$ballot->office->name}} --}}
                 <a class="underline text-sky-600 visited:text-purple-600" href="/ballot/{{$ballot->id}}">{{$ballot->location->name}} {{$ballot->office->name}}</a>
+                <div x-show="show" class="flex uppercase mt-2 text-sm justify-center">
+                    VOTING DATE: {{$ballot->voting_date}}
+                </div>
             </button>
         </form>
     @endforeach 
