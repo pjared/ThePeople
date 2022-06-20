0e386c6ab7ccc38745ffd29ddd1e03bc28642e62
pjared <pjared870@gmail.com>
2022-06-19 21:30:06 -0600

    initializes admin pages

---
+++ resources/views/admin/show.blade.php
@@ -0,0 +1,11 @@
+<x-app-layout>
+    <div class="grid grid-flow-row bg-gray-100 gap-4">
+        {{-- TODO: Keep this for expansion --}}
+        <div class="flex flex-row flex-wrap justify-center gap-6 mt-12">
+            {{-- <livewire:find-ballot class="flex flex-row gap-6 mt-12"/> --}}
+            <livewire:admin-page />
+        </div>
+        
+
+    </div>
+</x-app-layout>
