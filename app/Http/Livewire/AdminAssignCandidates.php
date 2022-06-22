ad14b9e19f254b3653d51cc12785276cefa5a8aa
pjared <pjared870@gmail.com>
2022-06-22 10:38:15 -0600

    Implements working admin assignment page

--- app/Http/Livewire/AdminAssignCandidates.php
+++ app/Http/Livewire/AdminAssignCandidates.php
@@ -4,6 +4,7 @@ namespace App\Http\Livewire;
 
 use App\Models\Ballot;
 use App\Models\Candidate;
+use App\Models\RunningCandidates;
 use Livewire\Component;
 
 class AdminAssignCandidates extends Component
@@ -11,13 +12,37 @@ class AdminAssignCandidates extends Component
     public $candidates;
     public $ballots;
 
+    public $office_id;
+    public $location_id;
+
+    public function assignCandidate($candidate_id)
+    {
+        // Attempt to find the ballot
+        $ballot = Ballot::where('office_id', $this->office_id)->where('location_id', $this->location_id)->first();
+
+        //Return if we don't find a ballot
+        if(!$ballot) {
+            session()->flash('error', 'Could not find this ballot');
+            return;
+        }
+
+        //Add to the ballot
+        $running_candidate = new RunningCandidates();
+        $running_candidate->candidate_id = $candidate_id;
+        $running_candidate->ballot_id = $ballot->id;
+        $running_candidate->save();
+
+        //Return if we don't find a ballot
+        session()->flash('message', 'Candidate has been added to ballot');
+    }
+
     public function mount() {
-        $this->candidates = Candidate::all();
         $this->ballots = Ballot::all();
     }
 
     public function render()
     {
+        $this->candidates = Candidate::all();
         return view('livewire.admin-assign-candidates');
     }
 }
--- resources/views/livewire/admin-assign-candidates.blade.php
+++ resources/views/livewire/admin-assign-candidates.blade.php
@@ -1,4 +1,16 @@
-<div class="flex grow justify-center">
+<div class="flex flex-col grow items-center">
+    <div class="flex w-screen justify-center">
+        @if (session()->has('message'))
+            <div class="p-4 mb-4 text-sm text-green-700 bg-green-100 rounded-lg dark:bg-green-200 dark:text-green-800 w-2/5" role="alert">
+                {{ session('message') }}
+            </div>
+        @endif
+        @if (session()->has('error'))
+            <div class="p-4 mb-4 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-red-200 dark:text-red-800 w-2/5" role="alert">
+                {{ session('error') }}
+            </div>
+        @endif
+    </div>
     @role('admin')
         <div class="w-11/12">
             <div class="flex grow flex-row gap-6">
@@ -11,22 +23,38 @@
                             @foreach ($candidates as $candidate)
                                 @if(!$candidate->ballot)
                                     <div class="flex flex-row card grow outline outline-1 outline-black gap-6 text-left">
-                                        <div class="flex flex-col">
+                                        <div class="flex grow flex-col">
                                             <div>
                                                 Name: {{$candidate->name}}
                                             </div>
                                             <div>
                                                 Email: {{$candidate->email}}
                                             </div>
+                                            <div>
+                                                Number: {{$candidate->phone_number}}
+                                            </div>
                                         </div>
-                                        <div class="flex flex-col">
+                                        <div class="flex grow flex-col">
                                             <div>
                                                 State: {{$candidate->state}}
                                             </div>
                                             <div>
-                                                Office: {{$candidate->office_name}} {{$candidate->location}}
+                                                Office: {{$candidate->application->office_name}} {{$candidate->application->location}}
                                             </div>   
                                         </div>
+
+                                        <!-- TODO: Finish up a form for assigning candidate to ballot-->
+                                        <form class="flex justify-end w-1/4" wire:keydown.enter="assignCandidate({{$candidate->id}})">
+                                            <div class="flex flex-col">
+                                                <label>Office ID</label>
+                                                <input class="text-input w-3/4" type="text" wire:model="office_id">
+                                            </div>
+                                            <div class="flex flex-col">
+                                                <label>Location ID</label>
+                                                <input class="text-input w-3/4" type="text" wire:model="location_id">
+                                            </div>
+                                        </form>
+                                        
                                     </div>        
                                 @endif
                             @endforeach
