cfe892df6ac377ebe7825b607e38fbeee4a0fc07
pjared <pjared870@gmail.com>
2022-06-26 13:41:47 -0600

    adds daisy UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,5 +23,6 @@ module.exports = {
         require('@tailwindcss/forms'), 
         require('@tailwindcss/typography'),
         require('@tailwindcss/nesting'),
+        require("daisyui"),
     ],
 };
