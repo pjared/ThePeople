891aa7230f41e51afb059dcfa7ba658f4ddf74a6
pjared <pjared870@gmail.com>
2022-06-28 09:20:07 -0600

    Can update/delete candidate promises

--- app/Http/Livewire/EditCandidatePromises.php
+++ app/Http/Livewire/EditCandidatePromises.php
@@ -10,6 +10,10 @@ class EditCandidatePromises extends Component
     public $candidate_id;
 
     public $promises;
+    
+    protected $listeners = [
+        'refreshPromiseComponent' => '$refresh',
+    ];
 
     protected $rules = [
         'promises.*.promise' => 'required',
@@ -20,6 +24,19 @@ class EditCandidatePromises extends Component
     {
         $this->candidate_id = $candidate_id;
         $this->promises = $promises;
+    }
+
+    public function update_promises()
+    {
+        $this->validate();
+        $this->emitUp('promise-update-flash');
+        $this->promises->each->save();
+    }
+
+    public function delete_promise($promise_id)
+    {
+        $this->promises->find($promise_id)->delete();
+        $this->emitUp('promise-delete-flash');
     }
 
     public function render()
--- resources/views/livewire/edit-candidate-promises.blade.php
+++ resources/views/livewire/edit-candidate-promises.blade.php
@@ -2,14 +2,14 @@
     <div class="flex">
         Your promises for this term in office
     </div>
-    <div x-data="{show:false}" class="flex grow flex-col w-11/12">
+    <div x-data="{show:false}" class="flex grow flex-col w-11/12 gap-2">
         @foreach ($promises as $i => $promise)
             <div class="flex grow flex-col items-center">
                 <div class="form-control w-full max-w-xs">
                     <label class="label">
                       <span class="label-text">Promise</span>
                     </label>
-                    <input type="text" wire:model="promises.{{ $i }}.promise" class="input input-bordered w-3/4 max-w-xs" />
+                    <input type="text" wire:model.defer="promises.{{ $i }}.promise" class="input input-bordered w-3/4 max-w-xs" />
                     <label class="label">
                         <span class="label-text">Plan</span>
                     </label>
@@ -20,8 +20,16 @@
                     @error('promises.'.$i.'.promise') {{ $message }} @enderror
                     @error('promises.'.$i.'.plan') {{ $message }} @enderror
                 </span>
+                <div class="flex grow flex-row justify-center">
+                    <button class="btn btn-error" wire:click="delete_promise({{$promise->id}})">Delete Promise</button>
+                </div>
             </div>
         @endforeach
+        @if(count($promises) >= 1)
+            <div class="flex grow justify-center">
+                <button class="btn btn-primary w-1/4" wire:click='update_promises'>Update Promises</button>
+            </div>                   
+        @endif        
         @if(count($promises) < 5)
             <livewire:candidate-add-promise :candidate_id="$candidate_id" :wire:key="'add-promise'.$candidate_id">                        
         @endif
