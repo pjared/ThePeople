c8c4323d672115882060804be14b412ba6df985d
pjared <pjared870@gmail.com>
2022-06-29 10:12:26 -0600

    position term limit is nullable

--- database/migrations/2022_05_02_014408_create_politicialofficepositions_table.php
+++ database/migrations/2022_05_02_014408_create_politicialofficepositions_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->tinyInteger('years_per_term')->unsigned();
-            $table->tinyInteger('limit_terms')->unsigned();
+            $table->tinyInteger('limit_terms')->unsigned()->nullable();
         });
     }
 
