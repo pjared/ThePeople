fe37a94ef6f23d5475aa8ca3daa37febaf68f3df
pjared <pjared870@gmail.com>
2022-06-30 11:18:42 -0600

    Starts an admin home page

--- resources/views/admin/show.blade.php
+++ resources/views/admin/show.blade.php
@@ -2,9 +2,28 @@
     <div class="grid grid-flow-row bg-gray-100 gap-4">
         @include('admin.admin-nav')
 
-        {{-- <div class="flex flex-row flex-wrap justify-center gap-6 mt-12">
-            <livewire:admin-approve-candidates />
-        </div> --}}
-        {{-- TODO: Write an admin home page --}}
+        <div class="grid grid-cols-3">
+            <div class="col-span-1">
+                <div class="grid grid-rows-2">
+                    <div class="row-span-1">
+                        <span>
+                            Approve new candidates
+                        </span>
+                        <p>
+                            This page will show you the most recent applicants for the site.
+                        </p>
+                    </div>
+                    <div class="row-span-1">
+                        <button class="btn btn-primary">
+                            <a href="{{ route('approve_candidates') }}">Go to page</a>
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-span-1">
+            </div>
+            <div class="col-span-1">
+            </div>
+        </div>
     </div>
 </x-app-layout>
