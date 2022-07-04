08a0bec9338b4c0236984ba97876c830fde400ef
pjared <pjared870@gmail.com>
2022-07-04 12:57:56 -0700

    Implements changing location opin type

--- app/Http/Livewire/AdminEditOpinions.php
+++ app/Http/Livewire/AdminEditOpinions.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire;
 
 use App\Models\ControversialOpinion;
+use App\Models\Location;
 use App\Models\OpinionTypes;
 use Livewire\Component;
 
@@ -10,20 +11,32 @@ class AdminEditOpinions extends Component
 {
     public $opinions;
     public $opinion_types;
+    public $locations;
 
     protected $rules = [
-        "opinions.*.type_id" => 'required',
+        "opinions.*.type_id" => 'required|numeric',
+        "locations.*.opinion_type_id" => 'required|numeric',
     ];
 
     public function save_opinions()
     {
+        $this->validate();
         $this->opinions->each->save();
+        session()->flash('message', 'Opinion IDs have been updated');
+    }
+
+    public function save_locations() 
+    {
+        $this->validate();
+        $this->locations->each->save();
+        session()->flash('message', 'Locations have been updated');
     }
 
     public function mount()
     {
         $this->opinions = ControversialOpinion::orderBy('type_id')->get();
         $this->opinion_types = OpinionTypes::all();
+        $this->locations = Location::all();
     }
 
     public function render()
--- resources/views/livewire/admin-edit-opinions.blade.php
+++ resources/views/livewire/admin-edit-opinions.blade.php
@@ -1,49 +1,87 @@
-<div class="flex flex-row justify-center gap-12">
+<div class="flex flex-col justify-center items-center gap-12">
     {{-- OPINIONS LIST --}}
-    <div class="flex flex-col text-center gap-4">
-        <span class="text-2xl">Opinions</span>
+    @if (session()->has('message'))
+        <div class="p-4 mb-4 text-sm text-green-700 bg-green-100 rounded-lg dark:bg-green-200 dark:text-green-800 w-3/4" role="alert">
+            {{ session('message') }}
+        </div>
+    @endif
+    @if (session()->has('error'))
+        <div class="p-4 mb-4 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-red-200 dark:text-red-800 w-3/4" role="alert">
+            {{ session('error') }}
+        </div>
+    @endif
     
+    <div class="grid grid-cols-3 justify-center gap-8">
+        {{-- OPINION LIST --}}
+        <div class="col-span-1 flex flex-col text-center gap-4">
+            <span class="text-2xl">Opinions</span>
+        
+            <form class="flex flex-col items-center" wire:keydown.enter="save_opinions">
+                @foreach ($opinions as $i => $opinion)
+                    <div class="grid grid-cols-2 gap-6 items-center justify-center">
+                        <div class="col-span-1 flex flex-col">
+                            <span class="text-xl">
+                                {{$opinion->name}}
+                            </span>
+                            <span>
+                                @if($opinion->type)
+                                    {{$opinion->type->name}}
+                                @else
+                                    (No Opinion Type)
+                                @endif
+                            </span>
+                        </div>
+                        
+                        <div class="col-span-1 flex flex-col items-center form-control">
+                            <label class="label">
+                                <span class="label-text">type_id</span>
+                            </label>
+                            <input type="number" wire:model.defer="opinions.{{$i}}.type_id" class="input input-bordered w-2/5" />
+                        </div>
+                        @error("opinions.{{$i}}.type_id") <span class="error">{{ $message }}</span> @enderror
+                    </div>
+                @endforeach
+            </form>
+        </div>
+    
+        {{-- OPINION TYPE LIST --}}
+        <div class="col-span-1 flex flex-col text-center gap-4">
+            <span class="text-2xl">Opinion Types</span>
+            @foreach ($opinion_types as $type)
+                <div class="flex flex-col">
+                    <span class="text-xl">
+                        {{$type->name}} ({{$type->id}})
+                    </span>
+                    <span>
+                        {{$type->description}}
+                    </span>
+                </div>
+            @endforeach
+        </div>
 
-        <form class="flex flex-col" wire:keydown.enter="save_opinions">
-            @foreach ($opinions as $i => $opinion)
-                <div class="flex flex-row gap-6 justify-center">
-                    <div class="flex flex-col">
+        {{-- LOCATION LIST --}}
+        <form class="col-span-1 flex flex-col items-center" wire:keydown.enter="save_locations">
+            <span class="text-2xl">Location & Types</span>
+            @foreach ($locations as $i => $location)
+                <div class="grid grid-cols-2 gap-6 items-center justify-center">
+                    <div class="col-span-1 flex flex-col items-center">
                         <span class="text-xl">
-                            {{$opinion->name}}
+                            {{$location->name}}, {{$location->state}}
                         </span>
                         <span>
-                            @if($opinion->type)
-                                {{$opinion->type->name}}
-                            @else
-                                (No Opinion Type)
-                            @endif
+                            {{$location->type}}
                         </span>
                     </div>
                     
-                    <div class="flex flex-col form-control">
+                    <div class="col-span-1 flex flex-col form-control items-center">
                         <label class="label">
                             <span class="label-text">type_id</span>
                         </label>
-                        <input type="number" wire:model.defer="opinions.{{$i}}.type_id" class="input input-bordered w-2/5" />
+                        <input type="number" wire:model.defer="locations.{{$i}}.opinion_type_id" class="input input-bordered w-2/5" />
                     </div>
-                    @error('opinions.{{$i}}.type_id') <span class="error">{{ $message }}</span> @enderror
+                    @error("locations.{{$i}}.opinion_type_id") <span class="error">{{ $message }}</span> @enderror
                 </div>
             @endforeach
         </form>
-    </div>
-
-    {{-- OPINION TYPE LIST --}}
-    <div class="flex flex-col text-center gap-4">
-        <span class="text-2xl">Opinion Types</span>
-        @foreach ($opinion_types as $type)
-            <div class="flex flex-col">
-                <span class="text-xl">
-                    {{$type->name}} ({{$type->id}})
-                </span>
-                <span>
-                    {{$type->description}}
-                </span>
-            </div>
-        @endforeach
-    </div>
+    </div>    
 </div>
