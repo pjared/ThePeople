b0d508e92cc13060b39412e1e20eff403e498a1a
pjared <pjared870@gmail.com>
2022-07-04 12:57:37 -0700

    Implements ability to change ballot for candidate

--- app/Http/Livewire/AdminAssignCandidates.php
+++ app/Http/Livewire/AdminAssignCandidates.php
@@ -16,11 +16,14 @@ class AdminAssignCandidates extends Component
     public $new_ballot_id;
 
     protected $rules = [
-        'placed_candidates.*.running_candidate.ballot_id' => 'required',
+        'placed_candidates.*.ballot_id' => 'required',
     ];
 
     public function assignCandidate($candidate_id)
     {
+        $this->validate([
+            'new_ballot_id' => 'required|numeric',
+        ]);
         // Attempt to find the ballot
         // $ballot = Ballot::where('office_id', $this->office_id)->where('location_id', $this->location_id)->first();
         $ballot = Ballot::find($this->new_ballot_id);
@@ -40,44 +43,22 @@ class AdminAssignCandidates extends Component
         session()->flash('message', 'Candidate has been added to ballot');
     }
 
-    // public function updateBallotAssignment()
-    // {
-    //     dd("um");
-
-    //     $this->validate();
-
- 
-
-    //     foreach ($this->placed_candidates as $post) {
-
-    //         $post->save();
-
-    //     }
-
-    //     $ballot = Ballot::find($this->ballot_id);
-
-    //     //Return if we don't find a ballot
-    //     if(!$ballot) {
-    //         session()->flash('error', 'Could not find this ballot');
-    //         return;
-    //     }
-
-    //     //Add to the ballot
-    //     $running_candidate = RunningCandidates::firstWhere('candidate_id', $candidate_id);
-    //     $running_candidate->ballot_id = $ballot->id;
-    //     $running_candidate->save();
-
-    //     session()->flash('message', 'Candidate has been added to ballot');
-    // }
+    public function updateBallotAssignment()
+    {
+        $this->validate();
+        //TODO: Make sure that it is a valid ballot ID
+        $this->placed_candidates->each->save();
+        session()->flash('message', 'Candidate has been updated');
+    }
 
     public function mount() {
         $this->ballots = Ballot::all();
+        $this->candidates = Candidate::doesntHave('ballot')->get();
+        $this->placed_candidates = RunningCandidates::orderBy('ballot_id')->get();
     }
 
     public function render()
     {
-        $this->candidates = Candidate::doesntHave('ballot')->get();
-        $this->placed_candidates = Candidate::has('ballot')->get();
         return view('livewire.admin-assign-candidates');
     }
 }
--- resources/views/livewire/admin-assign-candidates.blade.php
+++ resources/views/livewire/admin-assign-candidates.blade.php
@@ -17,6 +17,7 @@
             <div class="flex grow flex-row gap-6">
                 <div class="w-3/5 text-center">
                     <div class="flex grow flex-col gap-4">
+                        {{-- CANDIDATES TO PLACE --}}
                         <h1 class="text-xl">
                             Candidates To Place
                         </h1>
@@ -54,37 +55,40 @@
                                                 </label>
                                                 <input type="text" wire:model="new_ballot_id" class="input input-bordered w-3/4 max-w-xs" />
                                             </div>
+                                            @error('new_ballot_id') <span class="error">{{ $message }}</span> @enderror
+
                                         </form>
                                     </div>
                                 </div>
                             @endforeach
                         </div>
-                        {{-- <h1 class="text-xl">
+                        {{-- PLACED CANDIDATES --}}
+                        <h1 class="text-xl">
                             Candidates Placed
                         </h1>
                         <form class="flex grow flex-col background-card gap-1" wire:submit.prevent='updateBallotAssignment'>
-                            @foreach ($placed_candidates as $i => $candidate)
-                                <div wire:key="candidate-ballot-{{ $candidate->id }}" class="grid grid-cols-3 background-card outline outline-1 outline-black gap-6 text-left">
+                            @foreach ($placed_candidates as $i => $placed_candidate)
+                                <div wire:key="candidate-ballot-{{ $placed_candidate->candidate->id }}" class="grid grid-cols-3 background-card outline outline-1 outline-black gap-6 text-left">
                                     <div class="col-span-1">
                                         <div class="flex grow flex-col">
                                             <div>
-                                                Name: {{$candidate->name}}
+                                                Name: {{$placed_candidate->candidate->name}}
                                             </div>
                                             <div>
-                                                Email: {{$candidate->email}}
+                                                Email: {{$placed_candidate->candidate->contact_email}}
                                             </div>
                                             <div>
-                                                Number: {{$candidate->phone_number}}
+                                                Number: {{$placed_candidate->candidate->phone_number}}
                                             </div>
                                         </div>
                                     </div>
                                     <div class="col-span-1">
                                         <div class="flex grow flex-col">
                                             <div>
-                                                State: {{$candidate->state}}
+                                                State: {{$placed_candidate->candidate->state}}
                                             </div>
                                             <div>
-                                                Office: {{$candidate->application->office_name}} {{$candidate->application->location}}
+                                                {{-- Office: {{$placed_candidate->candidate->ballot->office->name}} {{$placed_candidate->candidate->ballot->location->name}} --}}
                                             </div>   
                                         </div>
                                     </div>
@@ -94,14 +98,15 @@
                                                 <label class="label">
                                                   <span class="label-text">Ballot ID</span>
                                                 </label>                                                
-                                                <input type="text" wire:model="placed_candidates.{{$i}}.running_candidate.ballot_id" class="input input-bordered w-3/4 max-w-xs" />
+                                                <input type="text" wire:model="placed_candidates.{{$i}}.ballot_id" class="input input-bordered w-3/4 max-w-xs" />
                                             </div>
+                                            @error('placed_candidates.{{$i}}.ballot_id') <span class="error">{{ $message }}</span> @enderror
                                         </div>
                                     </div>
                                 </div>
                             @endforeach
                             <button class="btn btn-primary" type="submit">Save</button>
-                        </form> --}}
+                        </form>
                     </div>                    
                 </div>
                 <div class="w-2/5">
