db18cd39f9cdf6813a1d10e90361749e5a4e3ee5
pjared <pjared870@gmail.com>
2022-07-04 16:53:39 -0700

    Updates candidate seeder for jetstream

--- database/seeders/CandidateSeeder.php
+++ database/seeders/CandidateSeeder.php
@@ -2,17 +2,22 @@
 
 namespace Database\Seeders;
 
+use App\Models\CandidateApplication;
+use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
 
 class CandidateSeeder extends Seeder
 {
     private $names = [
-        "Jared Pacheco",
+        "Lord Macbeth",
         "Gandalf",
-        "Ivan Fyodorvich",
+        "Cal Trask",
         "Cthulhu"
     ];
 
@@ -22,15 +27,35 @@ class CandidateSeeder extends Seeder
      * @return void
      */
     public function run()
-    {        
-        foreach ($this->names as $name) {
+    {   
+        foreach ($this->names as $name) {            
+            $user = User::create([
+                'name' => $name,
+                'email' => $name . '@gmail.com',
+                'password' => Hash::make('password'),
+                'profile_photo_path' => 'profile-photos/' . rand(1,3)  . '.jpg',
+            ]);
+            //Assign them to the candidate role
+            $user->assignRole('candidate');
+            $application = new CandidateApplication();
+            $application->user_id = $user->id;
+            $application->name = $user->name;
+            $application->dob = Carbon::now()->subYear(rand(10,100));
+            $application->email = $name . '@gmail.com';
+            $application->phone_number = '1234567891';
+            $application->office_name = 'Governer';
+            $application->state = 'Utah';
+            $application->location = 'District 12';
+            $application->status = 'accepted';
+            $application->save();
+
             DB::table('candidates')->insert([
                 'name' => $name,
                 'dob' => Carbon::today()->subYear(rand(25, 55)),
                 'signup_date' => Carbon::today()->subDays(rand(0, 365)),
                 'bio' => "", 
                 'party_id' => rand(1,3),
-                'image_id' => strval(rand(1,3)), 
+                'user_id' => $user->id, 
                 'state' => 'Utah',
                 'contact_email' => "pjared@gmail.com",
                 'political_leaning' => 'Centrist',
