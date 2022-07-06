0b57f13d5e5424ff4c13ccb53e1196f85be7a448
pjared <pjared870@gmail.com>
2022-07-06 10:03:02 -0700

    Documents candidate opinion edit

--- app/Http/Livewire/EditCandidateOpinions.php
+++ app/Http/Livewire/EditCandidateOpinions.php
@@ -19,16 +19,19 @@ class EditCandidateOpinions extends Component
     public function update_badges() 
     {
         foreach($this->stances as $stance) {
+            // If the stance is at 50, it's considered neutral
             if($stance->value == 50) {
                 $badge_name = $stance->opinion->name . ' - Nuetral';
                 $badge = Badge::firstWhere('name', $badge_name);
-                // dd($badge_name, Badge::all());
             } else if ($stance->value < 50) {
+                // < 50, get the first side of the opinion
                 $badge = Badge::firstWhere('name', $stance->opinion->first_side);
             } else {
+                // > 50, get the second ("right")side of the opinion
                 $badge = Badge::firstWhere('name', $stance->opinion->second_side);
             }
             
+            //Update the candidate badge if it exists, or create one if it doesn't
             CandidateBadge::updateOrCreate(
                 [
                     'candidate_id' => $stance->candidate_id,
@@ -50,9 +53,7 @@ class EditCandidateOpinions extends Component
     {
         $this->validate();
         
-        foreach($this->stances as $stance) {
-            $stance->save();
-        }
+        $this->stances->each->save();
         $this->update_badges();
         $this->emitUp('opinion-flash');
     }
