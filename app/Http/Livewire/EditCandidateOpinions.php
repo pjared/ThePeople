0e7a57b25fd53baf4899e63bcbfcce0b3c4d8530
pjared <pjared870@gmail.com>
2022-07-05 22:46:57 -0700

    Editing opinions sets candidate badges

--- app/Http/Livewire/EditCandidateOpinions.php
+++ app/Http/Livewire/EditCandidateOpinions.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Badge;
+use App\Models\CandidateBadge;
+use App\Models\ControversialOpinion;
 use Livewire\Component;
 
 class EditCandidateOpinions extends Component
@@ -12,6 +15,31 @@ class EditCandidateOpinions extends Component
         'stances.*.value' => 'required|numeric',
         'stances.*.info' => 'nullable'
     ];
+
+    public function update_badges() 
+    {
+        foreach($this->stances as $stance) {
+            if($stance->value == 50) {
+                $badge_name = $stance->opinion->name . ' - Nuetral';
+                $badge = Badge::firstWhere('name', $badge_name);
+                // dd($badge_name, Badge::all());
+            } else if ($stance->value < 50) {
+                $badge = Badge::firstWhere('name', $stance->opinion->first_side);
+            } else {
+                $badge = Badge::firstWhere('name', $stance->opinion->second_side);
+            }
+            
+            CandidateBadge::updateOrCreate(
+                [
+                    'candidate_id' => $stance->candidate_id,
+                    'opinion_id' => $stance->controversial_opinion_id
+                ],
+                [
+                    'badge_id' => $badge->id,
+                ]
+            );
+        }
+    }
 
     public function mount($stances)
     {
@@ -25,7 +53,7 @@ class EditCandidateOpinions extends Component
         foreach($this->stances as $stance) {
             $stance->save();
         }
-
+        $this->update_badges();
         $this->emitUp('opinion-flash');
     }
 
