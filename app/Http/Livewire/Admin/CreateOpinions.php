9918c56dfbe99a081a44388663a096726728652c
pjared <pjared870@gmail.com>
2022-07-09 11:38:30 -0700

    Removes creating opinion types

--- app/Http/Livewire/Admin/CreateOpinions.php
+++ app/Http/Livewire/Admin/CreateOpinions.php
@@ -2,30 +2,25 @@
 
 namespace App\Http\Livewire\Admin;
 
+use App\Models\BallotOpinions;
 use App\Models\ControversialOpinion;
-use App\Models\OpinionTypes;
 use Livewire\Component;
 
 class CreateOpinions extends Component
 {
 
     public ControversialOpinion $new_opinion;
-    public OpinionTypes $new_type;
 
     public $opinions;
-    public $opinion_types;
 
     protected $rules = [
         'new_opinion.name' => 'required',
         'new_opinion.description' => 'required',
         'new_opinion.votes' => 'required|numeric',
-        'new_type.name' => 'required',
-        'new_type.description' => 'required',
     ];
 
     public function mount() {
         $this->new_opinion = new ControversialOpinion();
-        $this->new_type = new OpinionTypes();
     }
 
     public function render()
@@ -36,10 +31,5 @@ class CreateOpinions extends Component
     public function add_opinion() {
         $this->validate();
         $this->new_opinion->save();
-    }
-
-    public function add_type() {
-        // $this->validate();
-        $this->new_type->save();
     }
 }
--- resources/views/livewire/Admin/create-opinions.blade.php
+++ resources/views/livewire/Admin/create-opinions.blade.php
@@ -26,39 +26,13 @@
                         <input type="number" wire:model.defer="new_opinion.votes" class="input input-bordered w-full max-w-xs" />
                     </div>
                     @error('new_opinion.votes') <span class="error">{{ $message }}</span> @enderror
-                    {{-- <div class="form-control w-full max-w-xs">
-                        <label class="label">
-                            <span class="label-text">type_id</span>
-                        </label>
-                        <input type="number" wire:model.defer="new_opinion.type_id" class="input input-bordered w-full max-w-xs" />
-                    </div>
-                    @error('new_opinion.type_id') <span class="error">{{ $message }}</span> @enderror --}}
                 </div>
                 <button wire:click='add_opinion' class="btn btn-primary mt-2">Add Opinion</button>
             </div>
         </div>
     </div>
     <div class="flex flex-col w-1/2 items-center gap-6">
-        {{-- OPINION TYPES --}}
-        <div class="flex flex-col background-card items-center rounded-t-none w-11/12" x-show="show" x-transition>
-            <h1>Create a new opinion type</h1>
-            <div class="flex flex-col grow ">
-                <div class="form-control w-full max-w-xs">
-                    <label class="label">
-                        <span class="label-text">Name</span>
-                    </label>
-                    <input type="text" wire:model.defer="new_type.name" class="input input-bordered w-full max-w-xs" />
-                </div>
-                @error('new_type.name') <span class="error">{{ $message }}</span> @enderror
-                <div class="form-control w-full max-w-xs">
-                    <label class="label">
-                        <span class="label-text">description</span>
-                    </label>
-                    <input type="text" wire:model.defer="new_type.description" class="input input-bordered w-full max-w-xs" />
-                </div>
-                @error('new_type.description') <span class="error">{{ $message }}</span> @enderror
-                <button wire:click='add_type' class="btn btn-primary mt-2">Add Opinion Type</button>
-            </div>
-        </div>
+        {{-- BALLOT OPINIONS --}}
+        
     </div>
 </div>
