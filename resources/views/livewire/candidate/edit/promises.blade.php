4c63547cd1cea5740cb9382d226eb8a8cdf36d48
pjared <pjared870@gmail.com>
2022-07-21 21:42:48 -0600

    Makes candidate edit promises a carousel

--- resources/views/livewire/candidate/edit/promises.blade.php
+++ resources/views/livewire/candidate/edit/promises.blade.php
@@ -3,28 +3,38 @@
         Your promises for reform/promises of personal values
     </div>
     <div x-data="{show:false}" class="flex grow flex-col w-11/12 gap-2">
-        @foreach ($promises as $i => $promise)
-            <div class="flex grow flex-col items-center">
-                <div class="form-control w-full max-w-xs">
-                    <label class="label">
-                      <span class="label-text">Promise</span>
-                    </label>
-                    <input type="text" wire:model.defer="promises.{{ $i }}.promise" class="input input-bordered w-3/4 max-w-xs" />
-                    <label class="label">
-                        <span class="label-text">Plan</span>
-                    </label>
-                    <textarea class="textarea textarea-bordered flex grow" wire:model.defer="promises.{{ $i }}.plan"></textarea>
+        <div class="carousel w-full">
+            @foreach ($promises as $i => $promise)
+                <div id="promise-{{$i}}" class="carousel-item w-full">
+                    <div class="flex grow flex-col items-center">
+                        <div class="form-control w-full max-w-xs">
+                            <label class="label">
+                            <span class="label-text">Promise</span>
+                            </label>
+                            <input type="text" wire:model.defer="promises.{{ $i }}.promise" class="input input-bordered w-3/4 max-w-xs" />
+                            <label class="label">
+                                <span class="label-text">Plan</span>
+                            </label>
+                            <textarea class="textarea textarea-bordered flex grow" wire:model.defer="promises.{{ $i }}.plan"></textarea>
+                        </div>
+                        {{-- TODO: ADD DELETE AND UPDATE BUTTONS --}}
+                        <span class="error">
+                            @error('promises.'.$i.'.promise') {{ $message }} @enderror
+                            @error('promises.'.$i.'.plan') {{ $message }} @enderror
+                        </span>
+                        <div class="flex grow flex-row justify-center">
+                            <button class="btn btn-error" wire:click="delete_promise({{$promise->id}})">Delete Promise</button>
+                        </div>
+                    </div>
                 </div>
-                {{-- TODO: ADD DELETE AND UPDATE BUTTONS --}}
-                <span class="error">
-                    @error('promises.'.$i.'.promise') {{ $message }} @enderror
-                    @error('promises.'.$i.'.plan') {{ $message }} @enderror
-                </span>
-                <div class="flex grow flex-row justify-center">
-                    <button class="btn btn-error" wire:click="delete_promise({{$promise->id}})">Delete Promise</button>
-                </div>
-            </div>
-        @endforeach
+            @endforeach            
+        </div>
+        <div class="flex justify-center w-full py-2 gap-2">
+            @foreach ($promises as $i => $promise)
+                <a href="#promise-{{$i}}" class="btn btn-xs">{{$i + 1}}</a>
+            @endforeach
+        </div>
+        
         @if(count($promises) >= 1)
             <div class="flex grow justify-center">
                 <button class="btn btn-primary w-1/4" wire:click='update_promises'>Update Promises</button>
@@ -38,3 +48,4 @@
         @endif
     </div>
 </div>
+
