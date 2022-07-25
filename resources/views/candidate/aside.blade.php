2a4ba38ff247cee92475574ef78eef2d0b234fbb
pjared <pjared870@gmail.com>
2022-07-25 11:41:51 -0600

    Creates candidate aside for navigating dashboard

--- resources/views/candidate/aside.blade.php
+++ resources/views/candidate/aside.blade.php
@@ -3,34 +3,7 @@
     >
     <div class="py-2 text-gray-500 ">
         <ul>
-            {{-- DASHBOARD --}}
-            <li class="relative px-6 py-3">
-                <span                
-                class="absolute inset-y-0 left-0 w-1 rounded-tr-lg rounded-br-lg 
-                {{ request()->routeIs('candidate-dashboard') ? 'bg-purple-600' : ''}}"
-                ></span>
-                <a
-                class="inline-flex items-center w-full text-sm font-semibold  transition-colors duration-150 hover:text-gray-800
-                {{ request()->routeIs('candidate-dashboard') ? ' text-gray-800' : ''}}"
-                href="/candidate/dashboard"
-                >
-                <svg
-                    class="w-5 h-5"
-                    aria-hidden="true"
-                    fill="none"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                >
-                    <path
-                    d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-                    ></path>
-                </svg>
-                <span class="ml-4">Candidate Dashboard</span>
-                </a>
-            </li>
+            
             {{-- PREVIEW PROFILE --}}
             <li class="relative px-6 py-3">
                 <span                
@@ -40,7 +13,7 @@
                 <a
                 class="inline-flex items-center w-full text-sm font-semibold  transition-colors duration-150 hover:text-gray-800
                 {{ request()->routeIs('candidate-preview') ? ' text-gray-800' : ''}}"
-                href="/candidate/preview/{{$candidate->id}}"
+                href="{{Route('candidate-preview')}}"
                 >
                     <svg
                         class="w-5 h-5"
@@ -146,6 +119,36 @@
                         </li>                            
                     </ul>
                     </div>
+            </li>
+
+            {{-- DASHBOARD --}}
+            <li class="relative px-6 py-3">
+                <span                
+                class="absolute inset-y-0 left-0 w-1 rounded-tr-lg rounded-br-lg 
+                {{ request()->routeIs('candidate-dashboard') ? 'bg-purple-600' : ''}}"
+                ></span>
+                <a
+                class="inline-flex items-center w-full text-sm font-semibold  transition-colors duration-150 hover:text-gray-800
+                {{ request()->routeIs('candidate-dashboard') ? ' text-gray-800' : ''}}"
+                
+                >
+                {{-- /candidate/dashboard --}}
+                <svg
+                    class="w-5 h-5"
+                    aria-hidden="true"
+                    fill="none"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                >
+                    <path
+                    d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+                    ></path>
+                </svg>
+                <span class="ml-4">Candidate Dashboard (Upcoming Feature)</span>
+                </a>
             </li>
 
             {{-- USER FLAGGING INFO --}}
