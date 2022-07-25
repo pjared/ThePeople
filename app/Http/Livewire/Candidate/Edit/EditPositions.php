d574eafcec3b40038271b3b8238715ae4fb9943a
pjared <pjared870@gmail.com>
2022-07-25 14:27:13 -0600

    Moves Positions into edit positions

--- app/Http/Livewire/Candidate/Edit/EditPositions.php
+++ app/Http/Livewire/Candidate/Edit/EditPositions.php
@@ -3,20 +3,67 @@
 namespace App\Http\Livewire\Candidate\Edit;
 
 use App\Models\Candidate;
+use App\Models\CandidateOfficePositions;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class EditPositions extends Component
 {
     public Candidate $candidate;
+    public CandidateOfficePositions $position;
+    public $positions;
+
+    protected $rules = [
+        'positions.*.position_name' => 'required',
+        'positions.*.year_start' => 'required|numeric',
+        'positions.*.year_end' => 'required|numeric',
+        'positions.*.description' => 'required',
+
+        'position.position_name' => 'required',
+        'position.year_start' => 'required|numeric',
+        'position.year_end' => 'required|numeric',
+        'position.description' => 'required',
+    ];
 
     public function mount()
     {
         $this->candidate = Candidate::firstWhere('user_id', Auth::id());
+
+        $this->positions = $this->candidate->previous_positions;
+        $this->position = new CandidateOfficePositions();
     }
 
     public function render()
     {
         return view('livewire.candidate.edit.edit-positions');
+    }
+
+    public function update_positions()
+    {
+        $this->validate([
+            'positions.*.position_name' => 'required',
+            'positions.*.year_start' => 'required|numeric',
+            'positions.*.year_end' => 'required|numeric',
+            'positions.*.description' => 'required'
+        ]);
+        
+        $this->emitUp('position-update-flash');
+        $this->positions->each->save();
+    }
+
+    public function delete_position($position_id)
+    {
+        $this->positions->find($position_id)->delete();
+        $this->emitUp('position-delete-flash');
+    }
+
+    public function add_position()
+    {
+        $this->validate();
+        
+        $this->position->candidate_id = $this->candidate_id;
+        $this->position->save();
+
+        $this->emitUp('position-flash');
     }
 }
--- resources/views/livewire/candidate/edit/edit-positions.blade.php
+++ resources/views/livewire/candidate/edit/edit-positions.blade.php
@@ -3,27 +3,79 @@
     
     <div class="flex flex-col w-full h-inherit items-center justify-center">
         {{-- PREVIOUS POSITIONS FLASH MESSAGE --}}
-        @if (session()->has('update-position-success'))
-            <div class="alert alert-success shadow-lg flex w-11/12">
-                <div>
-                    <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-                    <span>{{session('update-position-success')}}</span>
-                </div>
-            </div>
-        @elseif(session()->has('update-position-failure'))
-            <div class="alert alert-error shadow-lg">
-                <div>
-                    <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-                    <span>{{session('update-position-failure')}}</span>
-                </div>
-            </div>
-        @endif
+        @include('components.flash', ['name' => 'positions'])
+        
         <div class="flex flex-row w-full justify-between">
             {{-- LEFT COLUMN (Edit) --}}
             <div class="flex flex-col w-1/2 gap-2 p-4 items-center">
                 <h1 class="text-xl">Add your Previous Political Positions</h1>
-                {{-- PREVIOUS POSITIONS --}}                
-                <livewire:candidate.edit.positions :candidate_id="$candidate->id" :positions="$candidate->previous_positions" :wire:key="'candidate-positions'.$candidate->id">
+                {{-- PREVIOUS POSITIONS --}}            
+                <div class="flex flex-col background-card w-11/12 items-center">
+                    <div class="flex">
+                        Previous Political Positions
+                    </div>
+                    <div class="flex flex-col flex-grow w-11/12 gap-2">
+                        <div class="carousel w-full">
+                            @foreach ($positions as $i => $position)
+                                <div id="position-{{$i}}" class="carousel-item flex-col w-full">
+                                    <div class="flex grow flex-row gap-4">
+                                        <div class="form-control w-1/2 max-w-xs">
+                                            <label class="label">
+                                                <span class="label-text">Position Name</span>
+                                            </label>
+                                            <input type="text" wire:model.defer="positions.{{ $i }}.position_name" class="input input-bordered w-full max-w-xs" />
+                                            @error('positions.'.$i.'.position_name') {{ $message }} @enderror
+                                        </div>
+                                        @error('positions.{{ $i }}.position_name') <span class="error">{{ $message }}</span> @enderror    
+                                        <div class="flex grow justify-end">
+                                            <div class="form-control w-full max-w-xs">
+                                                <label class="label">
+                                                    <span class="label-text">Year Start</span>
+                                                </label>
+                                                <input type="number" wire:model.defer="positions.{{ $i }}.year_start" class="input input-bordered w-3/4 max-w-xs" />
+                                                @error('positions.'.$i.'.year_start') {{ $message }} @enderror
+                                            </div>         
+                                            @error('positions.{{ $i }}.year_start') <span class="error">{{ $message }}</span> @enderror
+                                            <div class="form-control w-full max-w-xs">
+                                                <label class="label">
+                                                    <span class="label-text">Year End</span>
+                                                </label>
+                                                <input type="number" wire:model.defer="positions.{{ $i }}.year_end" class="input input-bordered  w-3/4 max-w-xs" />
+                                            </div>
+                                            @error('positions.{{ $i }}.year_end') <span class="error">{{ $message }}</span> @enderror
+                                        </div>
+                                    </div>
+                                    <div class="form-control flex grow">
+                                        <label class="label">
+                                            <span class="label-text">Tell The People about your best accomplishments while in that office</span>
+                                        </label> 
+                                        <textarea class="textarea textarea-bordered flex grow" wire:model.defer="positions.{{ $i }}.description"></textarea>
+                                        @error('positions.'.$i.'.description') {{ $message }} @enderror
+                                    </div>
+                                    <div class="flex grow flex-row justify-center">
+                                        <button class="btn btn-error" wire:click="delete_position({{$position->id}})">Delete Positions</button>
+                                    </div>
+                                </div>
+                            @endforeach
+                        </div>
+                        <div class="flex justify-center w-full py-2 gap-2">
+                            @foreach ($positions as $i => $position)
+                                <a href="#position-{{$i}}" class="btn btn-xs">{{$i + 1}}</a>
+                            @endforeach
+                        </div>
+                        {{-- ADD A POSITION --}}
+                        @if(count($positions) >= 1)
+                            <div class="flex grow justify-center">
+                                <button class="btn btn-info w-1/4" wire:click='update_positions'>Update Positions</button>
+                            </div>                   
+                        @endif
+                        @if(count($positions) < 5)
+                            <label class="btn btn-primary" for="new-position">Add A Position</label>
+                
+                            @include('modals.new-position')
+                        @endif
+                    </div>    
+                </div>
             </div> 
             {{-- RIGHT COLUMN (Preview) --}}
             <div class="flex flex-col w-1/2 p-4 gap-6 items-center">
