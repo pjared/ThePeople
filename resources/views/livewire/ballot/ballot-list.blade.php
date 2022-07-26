4db8aebf34411edd581b6e46656f9cf0bf027ea3
pjared <pjared870@gmail.com>
2022-07-26 08:55:14 -0600

    Removes visited link from ballot list

--- resources/views/livewire/ballot/ballot-list.blade.php
+++ resources/views/livewire/ballot/ballot-list.blade.php
@@ -6,7 +6,7 @@
                 <button class="background-card" class="card bg-base-100 shadow-xl">
                     <div class="card-body text-center p-0">
                         <h2 class="card-title text-lg justify-center">
-                            <a class="underline text-sky-600 visited:text-purple-600" href="/ballot/{{$ballot->id}}"> {{$ballot->location->state}} {{$ballot->office->name}}, {{$ballot->location->name}} </a>
+                            <a class="underline text-sky-600" href="/ballot/{{$ballot->id}}"> {{$ballot->location->state}} {{$ballot->office->name}}, {{$ballot->location->name}} </a>
                         </h2>
                         <div x-show="show" class="uppercase mt-2 text-sm justify-center">
                             VOTING DATE: {{$ballot->voting_date->format('m/d/Y')}}
