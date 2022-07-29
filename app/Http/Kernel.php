a5bac66d0efcec80a169f20bfd34c2236184c941
pjared <pjared870@gmail.com>
2022-07-29 12:27:12 -0600

    Adds a middleware for 2FA

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -67,5 +67,7 @@ class Kernel extends HttpKernel
         'role' => \Spatie\Permission\Middlewares\RoleMiddleware::class,
         'permission' => \Spatie\Permission\Middlewares\PermissionMiddleware::class,
         'role_or_permission' => \Spatie\Permission\Middlewares\RoleOrPermissionMiddleware::class,
+
+        'has2FAEnabled' => \App\Http\Middleware\HasTwoFactorAuthEnabled::class,
     ];
 }
