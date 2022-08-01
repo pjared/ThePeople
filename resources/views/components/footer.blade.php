f7b93f642bc62de06ef8948e002ececfcdab49c3
pjared <pjared870@gmail.com>
2022-08-01 11:04:52 -0600

    Updates footer with new pages

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,26 +1,30 @@
 <footer class="footer p-10 bg-base-200 text-base-content">
     <div>
         <span class="text-3xl">ThePeople</span>
-        <p>by ComnRep, LLC.<br>What I had to name my company because ThePeople was taken</p>
-    </div> 
+        <p>by ComnRep, LLC.</p>
+    </div>
     {{-- <div>
-        <span class="footer-title">Services</span> 
-        <a class="link link-hover">Branding</a> 
-        <a class="link link-hover">Design</a> 
-        <a class="link link-hover">Marketing</a> 
+        <span class="footer-title">Services</span>
+        <a class="link link-hover">Branding</a>
+        <a class="link link-hover">Design</a>
+        <a class="link link-hover">Marketing</a>
         <a class="link link-hover">Advertisement</a>
     </div>  --}}
     <div>
-        <span class="footer-title">Company</span> 
-        <a class="link link-hover">About us (me)</a> 
-        <a class="link link-hover">Contact</a> 
+        <span class="footer-title">Company</span>
+        <a class="link link-hover" href="{{route('about-us')}}">About us (me)</a>
+        {{-- <a class="link link-hover">Contact</a> --}}
         {{-- <a class="link link-hover">Jobs</a>  --}}
         {{-- <a class="link link-hover">Press kit</a> --}}
-    </div> 
+    </div>
     <div>
-        <span class="footer-title">Legal</span> 
-        <a class="link link-hover">Terms of use</a> 
-        <a class="link link-hover">Privacy policy</a> 
-        <a class="link link-hover">Cookie policy</a>
+        <span class="footer-title">Legal</span>
+        <a class="link link-hover" href="{{route('terms.show')}}">Terms of use</a>
+        <a class="link link-hover" href="{{route('policy.show')}}">Privacy policy</a>
+        {{-- <a class="link link-hover">Cookie policy</a> --}}
+    </div>
+    <div>
+        <span class="footer-title">Contact</span>
+        <a class="">thepeople@whatsinyourballot.com</a>
     </div>
 </footer>
