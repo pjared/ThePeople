2ba8b36dd808be2cd9d2315d4b2c067e83530211
pjared <pjared870@gmail.com>
2022-08-02 19:18:23 -0600

    Adds table for permalinks

--- app/Http/Livewire/Admin/EditPermalinks.php
+++ app/Http/Livewire/Admin/EditPermalinks.php
@@ -9,15 +9,12 @@ use Livewire\Component;
 class EditPermalinks extends Component
 {
     public $candidates;
-    public $candidates_linked;
 
-    //TODO: Make sure that these are
     public $perma_link;
     public $candidate_link;
 
     public function save($candidate_id)
     {
-        // dd($candidate_id, $this->perma_link[$candidate_id], $this->candidate_link[$candidate_id]);
         $permaLink = new CandidatePermaLink();
         $permaLink->candidate_id = $candidate_id;
         $permaLink->perma_link = $this->perma_link[$candidate_id];
@@ -25,26 +22,9 @@ class EditPermalinks extends Component
         $permaLink->save();
     }
 
-    public function update($candidate_id)
-    {
-        $permaLink = CandidatePermaLink::firstWhere('candidate_id', $candidate_id);
-        // $permaLink->perma_link = $this->perma_link[$candidate_id]; Cannot change this
-        $permaLink->candidate_link = $this->candidate_link[$candidate_id];
-        $permaLink->save();
-    }
-
-    public function mount()
-    {
-        $this->candidates_linked = Candidate::has('permalink')->get();
-        foreach($this->candidates_linked as $candidate) {
-            $this->perma_link[$candidate->id] = $candidate->permalink->perma_link;
-            $this->candidate_link[$candidate->id] = $candidate->permalink->candidate_link;
-        }
-        $this->candidates = Candidate::doesntHave('permalink')->has('ballot')->get();
-    }
-
     public function render()
     {
+        $this->candidates = Candidate::doesntHave('permalink')->has('ballot')->get();
         return view('livewire.admin.edit-permalinks')
                     ->layout('layouts.admin');
     }
---
+++ app/Http/Livewire/Tables/CandidatePermalinksTable.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Livewire\Tables;
+
+use App\Models\CandidatePermaLink;
+use Mediconesystems\LivewireDatatables\Column;
+use Mediconesystems\LivewireDatatables\Http\Livewire\LivewireDatatable;
+use Mediconesystems\LivewireDatatables\NumberColumn;
+
+class CandidatePermalinksTable extends LivewireDatatable
+{
+    public $model = CandidatePermaLink::class;
+
+    public function columns()
+    {
+        return [
+            Column::name('candidate.name')
+                ->label('Candidate Name')
+                ->editable(),
+            NumberColumn::name('candidate_id')
+                ->label('Candidate ID')
+                ->editable(),
+            Column::name('candidate_link')
+                ->label('Candidate\'s Link')
+                ->editable(),
+            Column::name('perma_link')
+                ->label('Perma Link Ending')
+                ->editable(),
+        ];
+    }
+}
