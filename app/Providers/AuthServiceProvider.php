7dac63af90f2cd745e200bda771988c3808aa497
pjared <pjared870@gmail.com>
2022-08-08 16:53:07 -0600

    Adds location policy

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -7,12 +7,14 @@ use App\Models\BallotOpinions;
 use App\Models\Candidate;
 use App\Models\CandidatePermaLink;
 use App\Models\ControversialOpinion;
+use App\Models\Location;
 use App\Models\Team;
 use App\Policies\BallotOpinionsPolicy;
 use App\Policies\BallotPolicy;
 use App\Policies\CandidatePermaLinkPolicy;
 use App\Policies\CandidatePolicy;
 use App\Policies\ControversialOpinionsPolicy;
+use App\Policies\LocationPolicy;
 use App\Policies\TeamPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
@@ -30,6 +32,8 @@ class AuthServiceProvider extends ServiceProvider
         Ballot::class => BallotPolicy::class,
         CandidatePermaLink::class => CandidatePermaLinkPolicy::class,
         Candidate::class => CandidatePolicy::class,
+        Location::class => LocationPolicy::class,
+
     ];
 
     /**
