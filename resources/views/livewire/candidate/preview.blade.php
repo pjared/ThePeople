ac5f5c6f582ddf6a4445133700f43fba80e298c7
pjared <pjared870@gmail.com>
2022-08-09 17:40:58 -0600

    Updates the contact email for support

--- resources/views/livewire/candidate/preview.blade.php
+++ resources/views/livewire/candidate/preview.blade.php
@@ -2,7 +2,7 @@
     <div class="flex flex-col w-full">
         <div class="flex flex-col w-full gap-5 text-center items">
             @if($candidate->ballot)
-                <span>You will be placed on the {{$candidate->ballot->location->name}} {{$candidate->ballot->office->name}} ballot, in the state of {{$candidate->state}}. If this is incorrect, please contact customer support.</span>
+                <span>You will be placed on the {{$candidate->ballot->location->name}} {{$candidate->ballot->office->name}} ballot, in the state of {{$candidate->state}}. If this is incorrect, please contact support@whatsinyourballot.com.</span>
                 <div class="form-control items-center">
                     <label class="label cursor-pointer">
                         <span class="label-text">Show my profile on the ballot</span>
