727400b7649eb63c12417cd1b76d9d0367ac9ccd
pjared <pjared870@gmail.com>
2022-08-11 18:51:04 -0600

    Updates ballot assignment mail message

--- resources/views/emails/ballot-assigned.blade.php
+++ resources/views/emails/ballot-assigned.blade.php
@@ -1,3 +1,5 @@
 <div>
+   <p>Congrats! Your application has been approved. You may now access your candidate dashboard. Go to your profile at <a href="www.whatsinyourballot.com">www.whatsinyourballot.com</a> and a new button should appear at the top. We will assign your ballot soon. For now, you can start to fill out your profile.</p>
+
     <p>We've just assigned you to the ballot {{$ballot_name}}. You may now add your stances in your candidate dashboard. When your profile is finished, go to the "Preview Your Profile" section and select the show option at the top.</p>
 </div>
