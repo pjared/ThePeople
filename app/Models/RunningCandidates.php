c0b19b74f90eb96288dc31cc4a46db9734eff187
pjared <pjared870@gmail.com>
2022-08-12 12:14:12 -0600

    Adds comment about why model exists

--- app/Models/RunningCandidates.php
+++ app/Models/RunningCandidates.php
@@ -5,6 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * This model may seem unnecessary, and you may ask why this is not in the candidate model.
+ * When the candidates election is over and they run again next year, we can keep their old ballot profiles to use for accountability
+ */
 class RunningCandidates extends Model
 {
     use HasFactory;
