c8bbf4d4c9e2a65c3bb218e19859deea11eace8b
pjared <pjared870@gmail.com>
2022-08-15 19:18:43 -0600

    Updates newfeatures page for next test

--- app/Http/Livewire/NewFeature.php
+++ app/Http/Livewire/NewFeature.php
@@ -7,25 +7,9 @@ use Livewire\Component;
 
 class NewFeature extends Component
 {
+
     public function render()
     {
         return view('livewire.new-feature');
-    }
-
-    //Probably shouldn't submit these until the user hovers out of the area
-    public function change_flag($flag_type, $flag_id, $flag_value) {
-        //Update the userFlag variable
-        UserFlag::updateOrCreate(
-            [
-                'user_id' => 1,
-                'candidate_id' => 1,
-                'ballot_id' => 1,
-                'type' => $flag_type, // Promise, Donor, etc.
-                'type_id' => $flag_id, // The id of the promise, donor, etc.'
-            ],
-            [
-                'flag_type' => $flag_value,
-            ]
-        );
     }
 }
