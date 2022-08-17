5607a67117982e72b74a718e263845ccbd8789a3
pjared <pjared870@gmail.com>
2022-08-17 16:20:42 -0600

    Adds UF seeder to dbseeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -62,6 +62,8 @@ class DatabaseSeeder extends Seeder
 
                 RequiredStanceSeeder::class, //!PROD
                 CandidateRequiredStanceSeeder::class,
+
+                UserFlagSeeder::class,
             ]);
         }
     }
