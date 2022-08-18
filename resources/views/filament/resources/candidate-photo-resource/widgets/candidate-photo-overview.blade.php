3940a2012bff92237098f76f900e02d6a3d2596c
pjared <pjared870@gmail.com>
2022-08-18 13:39:32 -0600

    Adds preview line for photos

--- resources/views/filament/resources/candidate-photo-resource/widgets/candidate-photo-overview.blade.php
+++ resources/views/filament/resources/candidate-photo-resource/widgets/candidate-photo-overview.blade.php
@@ -1,5 +1,8 @@
 <x-filament::widget>
     <x-filament::card>
-        @include('candidate.component.photos', ['photos' => $candidate->photos])
+        <div class="flex flex-col p-4 gap-6 items-center">
+            <h1 class="text-xl">Photos Preview</h1>
+            @include('candidate.component.photos', ['photos' => $candidate->photos])
+        </div>
     </x-filament::card>
 </x-filament::widget>
