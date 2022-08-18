a932dd1427a9dc199b5bfe337449babc2fd9ffe1
pjared <pjared870@gmail.com>
2022-08-18 13:46:39 -0600

    Initializes event component

---
+++ resources/views/candidate/component/events.blade.php
@@ -0,0 +1,25 @@
+<div class="flex grow flex-col w-11/12 items-center">
+    @if (count($events) >= 1)
+        <div class='text-xl'>
+            Candidate's Upcoming Events
+        </div>
+
+        <div class="flex flex-col gap-4">
+            @foreach($events as $event)
+                <div class="flex flex-row justify-center gap-4">
+                    <div class="flex flex-col items-center gap-4">
+                        <span><b>{{ $event->event_name }}</b></span>
+                        <span>{{ $event->event_location }}</span>
+                        <span>{{ $event->event_description }}</span>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    @else
+        {{-- TODO: CHECK FOR MANUAL CANDIDATE --}}
+        <div>
+            Candidate does not have any events!
+        </div>
+    @endif
+
+</div>
