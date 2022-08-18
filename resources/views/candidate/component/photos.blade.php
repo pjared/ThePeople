07f13cc6c87a7c9716167d4f3aef79dcc13b642e
pjared <pjared870@gmail.com>
2022-08-18 12:29:51 -0600

    Updates photo candidate component

--- resources/views/candidate/component/photos.blade.php
+++ resources/views/candidate/component/photos.blade.php
@@ -1,26 +1,21 @@
-<div class="carousel rounded-box">
-    @foreach ($photos as $photo)
-        <div class="carousel-item">
-            <img src="{{ storage_path('public/' . $photo->attachment)}}" alt="Burger" />
-        </div>
-    @endforeach
-
-    {{-- <div class="carousel-item">
-      <img src="https://placeimg.com/400/300/arch" alt="Burger" />
+<div class="flex w-full justify-center">
+    <div class="carousel w-3/4 h-48">
+        @foreach ($photos as $i => $photo)
+            <div id="slide{{$i}}" class="carousel-item relative w-full items-center justify-center">
+                <img src="{{ Storage::url($photo->attachment)}}" class="h-48" />
+                <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    @if($loop->first)
+                        <a href="#slide{{count($photos) - 1}}" class="btn btn-circle"><</a>
+                    @else
+                        <a href="#slide{{$i - 1}}" class="btn btn-circle"><</a>
+                    @endif
+                    @if($loop->last)
+                        <a href="#slide0" class="btn btn-circle">></a>
+                    @else
+                        <a href="#slide{{$i + 1}}" class="btn btn-circle">></a>
+                    @endif
+                </div>
+            </div>
+        @endforeach
     </div>
-    <div class="carousel-item">
-      <img src="https://placeimg.com/400/300/arch" alt="Burger" />
-    </div>
-    <div class="carousel-item">
-      <img src="https://placeimg.com/400/300/arch" alt="Burger" />
-    </div>
-    <div class="carousel-item">
-      <img src="https://placeimg.com/400/300/arch" alt="Burger" />
-    </div>
-    <div class="carousel-item">
-      <img src="https://placeimg.com/400/300/arch" alt="Burger" />
-    </div>
-    <div class="carousel-item">
-      <img src="https://placeimg.com/400/300/arch" alt="Burger" />
-    </div> --}}
 </div>
