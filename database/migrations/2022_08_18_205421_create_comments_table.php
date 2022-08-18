e7e5556f47040d5bb03d3054aeea44028ad1a3b8
pjared <pjared870@gmail.com>
2022-08-18 15:11:27 -0600

    Adds comment model, config, and migration

---
+++ database/migrations/2022_08_18_205421_create_comments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->morphs('commentable');
+            $table->text('comment');
+            $table->boolean('is_approved')->default(false);
+            $table->foreignIdFor(User::class, 'user_id')->nullable();
+            $table->string('deletion_reason')->nullable();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
