74a68f203e0b89cc2c7b3937f5ee3879832b697e
pjared <pjared870@gmail.com>
2022-08-18 21:12:14 -0600

    Adds replies to seeding

--- database/seeders/CommentSeeder.php
+++ database/seeders/CommentSeeder.php
@@ -20,16 +20,19 @@ class CommentSeeder extends Seeder
         $candidate->comment('Wow such a cool website');
         $comment = Comment::find(1);
         $comment->is_approved = true;
+        $comment->reply = 'Thanks man!';
         $comment->save();
 
         $candidate->comment('Wow and the website has such a nice background');
         $comment = Comment::find(2);
         $comment->is_approved = true;
+        $comment->reply = 'I spent a lot of time working on that one';
         $comment->save();
 
         $candidate->comment('The color scheme is off the charts');
         $comment = Comment::find(3);
         $comment->is_approved = true;
+        $comment->reply = 'I believe that the color schema does create a new atmosphere';
         $comment->save();
 
         $candidate->comment('And the UX skills? Don\'t get me starteds');
