419b9569b4e2c672ee8b4fc579890cc7a0c1f1ad
pjared <pjared870@gmail.com>
2022-08-18 20:58:44 -0600

    Updates comment seeder for pinned comments

--- database/seeders/CommentSeeder.php
+++ database/seeders/CommentSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Candidate;
+use App\Models\Comment;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -17,5 +18,23 @@ class CommentSeeder extends Seeder
     {
         $candidate = Candidate::firstWhere('user_id', 1);
         $candidate->comment('Wow such a cool website');
+        $comment = Comment::find(1);
+        $comment->is_approved = true;
+        $comment->save();
+
+        $candidate->comment('Wow and the website has such a nice background');
+        $comment = Comment::find(2);
+        $comment->is_approved = true;
+        $comment->save();
+
+        $candidate->comment('The color scheme is off the charts');
+        $comment = Comment::find(3);
+        $comment->is_approved = true;
+        $comment->save();
+
+        $candidate->comment('And the UX skills? Don\'t get me starteds');
+        $comment = Comment::find(4);
+        $comment->is_approved = true;
+        $comment->save();
     }
 }
