45c59dcc28c7d7e4e523d31c8affc6f4ddfd0a20
pjared <pjared870@gmail.com>
2022-08-22 10:47:25 -0600

    Creates group ballot questions model

---
+++ app/Models/GroupBallotQuestions.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class GroupBallotQuestions extends Model
+{
+    use HasFactory;
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'ballot_id',
+        'candidate_id',
+        'question',
+    ];
+}
---
+++ database/migrations/2022_08_22_030622_create_group_ballot_questions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\Ballot;
+use App\Models\PoliticalGroup;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('group_ballot_questions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(PoliticalGroup::class, 'political_group_id');
+            $table->foreignIdFor(Ballot::class, 'ballot_id');
+            $table->string('question');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('group_ballot_questions');
+    }
+};
