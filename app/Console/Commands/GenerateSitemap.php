d57384d7bcb02593ab4d3a63c9f3c183389ad7b4
pjared <pjared870@gmail.com>
2022-08-23 13:37:30 -0600

    Adds a generate sitemap artisan command

--- app/Console/Commands/GenerateSitemap.php
+++ app/Console/Commands/GenerateSitemap.php
@@ -2,8 +2,12 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Ballot;
+use Carbon\Carbon;
 use Illuminate\Console\Command;
+use Spatie\Sitemap\Sitemap;
 use Spatie\Sitemap\SitemapGenerator;
+use Spatie\Sitemap\Tags\Url;
 
 class GenerateSitemap extends Command
 {
@@ -28,7 +32,22 @@ class GenerateSitemap extends Command
      */
     public function handle()
     {
-        SitemapGenerator::create(config('app.url'))
-            ->writeToFile(public_path('sitemap.xml'));
+        $sitemap = Sitemap::create()
+                        ->add(Url::create(config('app.url')));
+        $ballots = Ballot::with(['candidates.candidate'])->get();
+
+        foreach($ballots as $ballot) {
+            $sitemap->add(Url::create('ballot/' . $ballot->slug)
+                            ->setPriority(0.5)
+                            ->setChangeFrequency('weekly'));
+            foreach($ballot->candidates as $candidate) {
+                $sitemap->add(Url::create('candidate/profile/' . $candidate->candidate->slug)
+                                ->setPriority(0.9)
+                                ->setChangeFrequency('weekly')
+                                ->setLastModificationDate($candidate->updated_at));
+            }
+        }
+
+        $sitemap->writeToFile(public_path('sitemap.xml'));
     }
 }
