1bc0a9c7a6c980eba7e49997c9c7fea2b586c217
pjared <pjared870@gmail.com>
2022-08-23 10:05:07 -0600

    New Feature flag test part 2

--- app/Http/Livewire/NewFeature.php
+++ app/Http/Livewire/NewFeature.php
@@ -12,4 +12,37 @@ class NewFeature extends Component
     {
         return view('livewire.new-feature');
     }
+
+    public function change_flag($flag_type, $flag_id, $flag_value, $flag_note) {
+        if(! auth()) {
+            return;
+        }
+        //Update the userFlag variable
+        UserFlag::updateOrCreate(
+            [
+                'user_id' => auth()->id(),
+                'candidate_id' => 5,
+                'ballot_id' => 1,
+                'type' => $flag_type, // Promise, Donor, etc.
+                'type_id' => $flag_id, // The id of the promise, donor, etc.'
+            ],
+            [
+                'flag_type' => $flag_value,
+                'note' => $flag_note,
+            ]
+        );
+    }
+
+    public function delete_flag($flag_type, $flag_id)
+    {
+        if(! auth()) {
+            return;
+        }
+        UserFlag::where('user_id', auth()->id())
+                        ->where('candidate_id', 5)
+                        ->where('ballot_id', 1)
+                        ->where('type',$flag_type)
+                        ->where('type_id',$flag_id)
+                        ->delete();
+    }
 }
--- resources/views/livewire/new-feature.blade.php
+++ resources/views/livewire/new-feature.blade.php
@@ -1,2 +1,54 @@
 <div class="flex justify-center items-center w-full h-full p-12 pt-40">
+
+
+    <livewire:flag :type="'test'" :type_id="1" :side="'left'" :wire:key="'test' . 1">
+
+    <div class='w-full flex justify-end'>
+        <div class='dropdown dropdown-top dropdown-end'>
+            <livewire:flag :type="'test-right'" :type_id="1" :side="'right'" :wire:key="'test-right' . 1">
+        </div>
+    </div>
+
+    @push('scripts')
+        <script>
+            document.addEventListener('alpine:init', () => {
+                Alpine.data('flag', () => ({
+                    new_changes: false,
+                    color:0,
+
+                    setColor(color) {
+                        if(color == '1') {
+                            this.color = 1;
+                            this.current_color = 'fill-red-600';
+                        } else if (color == '2') {
+                            this.color = 2;
+                            this.current_color = 'fill-green-600';
+                        } else if (color == '3') {
+                            this.color = 3;
+                            this.current_color = 'fill-gray-600';
+                        } else {
+                            this.color = 0;
+                            this.current_color = 'fill-transparent';
+                        }
+                        this.new_changes = true;
+                    },
+                    left() {
+                        console.log('saved');
+                        if(this.new_changes) {
+                            @this.change_flag(this.type, this.type_id, this.color, this.note);
+                            this.new_changes = false;
+                        }
+                    },
+                    deleteFlag() {
+                        @this.delete_flag(this.type, this.type_id)
+                    },
+                    changedNote() {
+                        console.log('Changed Note');
+                        this.new_changes = true;
+                    },
+                }))
+            });
+
+        </script>
+    @endpush
 </div>
