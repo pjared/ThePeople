44740e6a0c9d51f977f4c27ef88742cbdd348e7e
pjared <pjared870@gmail.com>
2022-08-24 09:31:17 -0600

    Adds a group application model

---
+++ app/Models/GroupApplication.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class GroupApplication extends Model
+{
+    use HasFactory;
+
+    protected $primaryKey = 'user_id';
+    public $incrementing = false;
+
+    protected $fillable = [
+        'user_id',
+        'description',
+        "name",
+        "email",
+        "state",
+        "location",
+        "status",
+        'group_id',
+    ];
+}
---
+++ database/migrations/2022_08_24_001529_create_group_applications_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Models\PoliticalGroup;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('group_applications', function (Blueprint $table) {
+            $table->foreignIdFor(User::class, 'user_id');
+            $table->string('name');
+            $table->text('description');
+            $table->string('email');
+            $table->string('state');
+            $table->string('location');
+            $table->string('status', 15);
+            $table->foreignIdFor(PoliticalGroup::class, 'group_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('group_applications');
+    }
+};
