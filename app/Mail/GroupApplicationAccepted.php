2b74f60f9adf9c2d88e12e6009dda998b6dc6c43
pjared <pjared870@gmail.com>
2022-08-26 11:55:03 -0600

    Adds queing to group emails

--- app/Mail/GroupApplicationAccepted.php
+++ app/Mail/GroupApplicationAccepted.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class GroupApplicationAccepted extends Mailable
+class GroupApplicationAccepted extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
--- app/Mail/GroupApplicationSubmitted.php
+++ app/Mail/GroupApplicationSubmitted.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class GroupApplicationSubmitted extends Mailable
+class GroupApplicationSubmitted extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
