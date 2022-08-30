6022ac20e7796392de43fb1d4d8acc04e4586d01
pjared <pjared870@gmail.com>
2022-08-30 11:41:25 -0600

    Moves flaggable class

--- app/Traits/Flaggable.php
+++ app/Traits/Flaggable.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace App\Traits;
+
 use App\Models\Flag;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
@@ -23,9 +25,9 @@ trait Flaggable
      * @param $candidate_id
      * @return \Illuminate\Database\Eloquent\Model
      */
-    public function flag($candidate_id, $ballot_id, string $note)
+    public function flag(string $note, $flag_type) //, $candidate_id, $ballot_id
     {
-        return $this->commentAsUser(auth()->user(), $note, $candidate_id, $ballot_id);
+        return $this->commentAsUser(auth()->user(), $note, $flag_type);
     }
 
     /**
@@ -35,15 +37,16 @@ trait Flaggable
      * @param string $comment
      * @return \Illuminate\Database\Eloquent\Model
      */
-    public function flagAsUser(?Model $user, string $note, $candidate_id, $ballot_id)
+    public function flagAsUser(?Model $user, string $note, $flag_type) //, $candidate_id, $ballot_id
     {
         $flag = new Flag([
             'user_id' => is_null($user) ? null : $user->getKey(),
-            'candidate_id' => $candidate_id,
-            'ballot_id' => $ballot_id,
+            // 'candidate_id' => $candidate_id,
+            // 'ballot_id' => $ballot_id,
             'note' => $note,
             'flaggable_id' => $this->getKey(),
             'flaggable_type' => get_class(),
+            'flag_type' => $flag_type
         ]);
 
         return $this->flags()->save($flag);
