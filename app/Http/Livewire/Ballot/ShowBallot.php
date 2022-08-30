17ba4a88fc8411d4516fc25f6b980be9d111e930
pjared <pjared870@gmail.com>
2022-08-30 10:24:07 -0600

    Adds ability for groups to back candidates

--- app/Http/Livewire/Ballot/ShowBallot.php
+++ app/Http/Livewire/Ballot/ShowBallot.php
@@ -4,6 +4,7 @@ namespace App\Http\Livewire\Ballot;
 
 use App\Models\Ballot;
 use App\Models\GroupBallotQuestions;
+use App\Models\PoliticalGroupCandidates;
 use App\Models\UserVotes;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
@@ -15,12 +16,20 @@ class ShowBallot extends Component
     public $candidate_vote;
     public $group_question;
 
+    public $backed_candidate;
+
     public function mount(Ballot $ballot)
     {
         if(Auth::user()) {
             $user_vote = UserVotes::where('ballot_id', $ballot->id)->where('user_id', Auth::id())->first();
             if($user_vote) {
                 $this->candidate_vote = $user_vote->running_candidate_id;
+            }
+            if(auth()->user()->manages_political_groups->first()) {
+                $group_candidate = PoliticalGroupCandidates::where('political_group_id', auth()->user()->manages_political_groups->first()->id)->firstWhere('ballot_id', $ballot->id);
+                if($group_candidate) {
+                    $this->backed_candidate = $group_candidate->candidate_id;
+                }
             }
         }
         $this->ballot = $ballot->load('location:id,name,state', 'office:id,name', 'candidates', 'candidates.candidate');
@@ -41,6 +50,27 @@ class ShowBallot extends Component
                 'is_valid' => 1,
             ]
         );
+    }
+
+    public function change_backing() {
+        PoliticalGroupCandidates::updateOrCreate(
+            [
+                'political_group_id' => auth()->user()->manages_political_groups->first()->id,
+                'ballot_id' => $this->ballot->id,
+                'candidate_id' => $this->backed_candidate,
+            ]
+            );
+
+    }
+
+    public function remove_backing()
+    {
+        $candidate = PoliticalGroupCandidates::where('political_group_id', auth()->user()->manages_political_groups->first()->id)
+                                    ->firstWhere('ballot_id', $this->ballot->id);
+        if($candidate) {
+            $candidate->delete();
+        }
+        $this->backed_candidate = '';
     }
 
     public function add_question() {
