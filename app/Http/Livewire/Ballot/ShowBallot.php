9e024bf465f7d19572a2c72a3691f9ded045ea2a
pjared <pjared870@gmail.com>
2022-08-30 10:40:10 -0600

    Moves group stuff to its own wire

--- app/Http/Livewire/Ballot/ShowBallot.php
+++ app/Http/Livewire/Ballot/ShowBallot.php
@@ -14,9 +14,6 @@ class ShowBallot extends Component
     public Ballot $ballot;
 
     public $candidate_vote;
-    public $group_question;
-
-    public $backed_candidate;
 
     public function mount(Ballot $ballot)
     {
@@ -24,12 +21,6 @@ class ShowBallot extends Component
             $user_vote = UserVotes::where('ballot_id', $ballot->id)->where('user_id', Auth::id())->first();
             if($user_vote) {
                 $this->candidate_vote = $user_vote->running_candidate_id;
-            }
-            if(auth()->user()->manages_political_groups->first()) {
-                $group_candidate = PoliticalGroupCandidates::where('political_group_id', auth()->user()->manages_political_groups->first()->id)->firstWhere('ballot_id', $ballot->id);
-                if($group_candidate) {
-                    $this->backed_candidate = $group_candidate->candidate_id;
-                }
             }
         }
         $this->ballot = $ballot->load('location:id,name,state', 'office:id,name', 'candidates', 'candidates.candidate');
@@ -50,45 +41,6 @@ class ShowBallot extends Component
                 'is_valid' => 1,
             ]
         );
-    }
-
-    public function change_backing() {
-        PoliticalGroupCandidates::updateOrCreate(
-            [
-                'political_group_id' => auth()->user()->manages_political_groups->first()->id,
-                'ballot_id' => $this->ballot->id,
-                'candidate_id' => $this->backed_candidate,
-            ]
-            );
-
-    }
-
-    public function remove_backing()
-    {
-        $candidate = PoliticalGroupCandidates::where('political_group_id', auth()->user()->manages_political_groups->first()->id)
-                                    ->firstWhere('ballot_id', $this->ballot->id);
-        if($candidate) {
-            $candidate->delete();
-        }
-        $this->backed_candidate = '';
-    }
-
-    public function add_question() {
-        if(auth()->user() && auth()->user()->hasRole('organizerAdmin')) {
-            //Check the users group
-            $user_group = auth()->user()->manages_political_groups->first();
-            if(!$user_group) {
-                return;
-            }
-            //Create the new model
-            $ballot_question = new GroupBallotQuestions();
-            $ballot_question->ballot_id = $this->ballot->id;
-            $ballot_question->political_group_id = $user_group->id;
-            $ballot_question->question = $this->group_question;
-            $ballot_question->save();
-            //Reset the text
-            $this->group_question = "";
-        }
     }
 
     public function render()
--- resources/views/livewire/ballot/show-ballot.blade.php
+++ resources/views/livewire/ballot/show-ballot.blade.php
@@ -16,33 +16,7 @@
             {{-- <livewire:ballot.show :ballot="$ballot"> --}}
             @include('ballot.show')
             @role('organizerAdmin')
-                <div class='flex flex-row'>
-                    <div class="flex flex-col grow gap-6 items-center w-1/2">
-                        <h2>Submit a question to the ballot</h2>
-                        <textarea wire:model.defer="group_question" class="textarea textarea-primary w-3/4" placeholder="Your question"></textarea>
-                        <button wire:click="add_question" class="btn btn-primary">Submit</button>
-                    </div>
-
-                    <div class='flex flex-col w-1/2 justify-center items-center gap-4'>
-                        {{-- <span>Currently Backing: </span> --}}
-                        {{-- {{dd('hi', $backed_candidate)}} --}}
-                        <select wire:model.defer='backed_candidate' class="select select-bordered w-full max-w-xs">
-                            <option selected value="">Back a Candidate</option>
-                            @foreach ($ballot->candidates as $running_candidate)
-                                <option value="{{$running_candidate->candidate->id}}">{{$running_candidate->candidate->name}}</option>
-                            @endforeach
-                        </select>
-                        <div class='flex flex-row gap-4'>
-                            <button wire:click="change_backing" class="btn btn-primary">Submit</button>
-                            @if($backed_candidate)
-                                <button wire:click="remove_backing" class="btn btn-error">Remove Current</button>
-                            @endif
-                        </div>
-                    </div>
-                </div>
-
-
-                {{--  --}}
+                <livewire:groups.group-ballot :ballot="$ballot"/>
             @endrole
         </div>
     </div>
