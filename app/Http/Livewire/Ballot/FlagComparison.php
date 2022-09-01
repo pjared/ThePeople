0f968687ed4f7fddd7768a52c797996d62160400
pjared <pjared870@gmail.com>
2022-09-01 16:23:31 -0600

    Flag comparison will load flaggable relationship

--- app/Http/Livewire/Ballot/FlagComparison.php
+++ app/Http/Livewire/Ballot/FlagComparison.php
@@ -20,13 +20,16 @@ class FlagComparison extends Component
     }
 
     public function mount(Ballot $ballot){
+        //TODO: load ballot candidates with info
+        // $ballot = $ballot->load('')
+
         // Get the user's vote
         $user_vote = UserVotes::where('ballot_id', $ballot->id)->where('user_id', auth()->id())->first();
         if($user_vote) {
             $this->candidate_vote = $user_vote->running_candidate_id;
         }
 
-        $this->flags = auth()->user()->flags->where('ballot_id', $ballot->id);
+        $this->flags = auth()->user()->flags->where('ballot_id', $ballot->id)->load('flaggable');
         $this->candidates = Candidate::whereRelation('running_candidate', 'ballot_id', $ballot->id)
                                         ->whereRelation('running_candidate','show', true)
                                         ->with(['stances', 'required_stances'])
