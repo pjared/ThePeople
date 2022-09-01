88fc77feb8a512a4f47b36f8f0740ca0eb2bd116
pjared <pjared870@gmail.com>
2022-09-01 16:25:42 -0600

    Updates flaginfo wire for new flag model

--- app/Http/Livewire/Candidate/FlagInfo.php
+++ app/Http/Livewire/Candidate/FlagInfo.php
@@ -8,22 +8,32 @@ class FlagInfo extends Component
 {
     public $promise_flags;
     public $opinion_flags = [];
-    public $position_flags = [];
+    public $position_flags;
+    public $other_opinion_flags;
 
     public function render()
     {
         return view('livewire.candidate.flag-info');
     }
 
-    public function mount($candidate_id, $flags, $opinions)
+    public function mount($flags, $opinions)
     {
-        $this->promise_flags = $flags->where('type', 'promise');
+        // dd($flags);
+        //Filter for the flags with promise
+        $this->promise_flags = $flags->where('flaggable_type', 'App\Models\CandidatePromise');
+        //Filter flags for each opinion in ballot
         foreach ($opinions as $opinion) {
-            $opinion_flags = $flags->where('type', $opinion->id . '-controversial-stance');
+            $opinion_flags = $flags->where('flaggable_type', 'App\Models\CandidateStance')
+                                        ->where('flaggable.controversial_opinion_id', $opinion->id);
             if(count($opinion_flags) >= 1) {
                 $this->opinion_flags[$opinion->name] = $opinion_flags;
             }
         }
-        $this->position_flags = $flags->where('type', 'position');
+        //Filter flags for previous positions
+        $this->position_flags = $flags->where('flaggable_type', 'App\Models\CandidateOfficePositions');
+
+        //Other Opinions
+        $this->other_opinion_flags = $flags->where('flaggable_type', 'App\Models\CandidateOpinions');
+        // dd($this->other_opinion_flags);
     }
 }
