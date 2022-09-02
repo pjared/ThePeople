8faedb9491b79263481c308fe38fd7a96ce6ae09
pjared <pjared870@gmail.com>
2022-09-02 16:15:31 -0600

    Adds a job for caching single ballot

---
+++ app/Jobs/UpdateBallotCache.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Ballot;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Cache;
+
+class UpdateBallotCache implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public Ballot $ballot;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(Ballot $ballot)
+    {
+        $this->ballot = $ballot;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $ballot = $this->ballot;
+        Cache::forget('ballot-' . $ballot->slug);
+        Cache::rememberForever('ballot-' . $ballot->slug, function () use ($ballot) {
+                    return $ballot->load('location:id,name,state',
+                                            'office:id,name',
+                                            'candidates',
+                                            'candidates.candidate:id,slug,profile_photo_path,name');
+                });
+    }
+}
