850e8eeb7e3d89ba5e46a23d1c546464fc02f4b4
pjared <pjared870@gmail.com>
2022-09-02 17:47:55 -0600

    Adds candidate cache job

---
+++ app/Jobs/UpdateCandidateCache.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Candidate;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Cache;
+
+class UpdateCandidateCache implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public Candidate $candidate;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(Candidate $candidate)
+    {
+        $this->candidate = $candidate;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $candidate = $this->candidate;
+        Cache::forget('candidate-' . $candidate->slug);
+        Cache::rememberForever('candidate-' . $candidate->slug, function () use ($candidate) {
+                    return $candidate->load('ballot', 'ballot.office:id,name', 'ballot.location:id,state,name',
+                                        'events', 'required_stances', 'stances', 'promises', 'videos', 'previous_positions');
+                });
+    }
+}
