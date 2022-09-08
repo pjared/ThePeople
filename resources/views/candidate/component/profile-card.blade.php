8807a9e5542d586fdd51f66c96f1c80d3c5837e5
pjared <pjared870@gmail.com>
2022-09-08 11:45:33 -0600

    Adds a layout just for candidate profile

---
+++ resources/views/candidate/component/profile-card.blade.php
@@ -0,0 +1,26 @@
+<article
+    class='background-card w-fit grid grid-flow-col grid-col-3 justify-center gap-4 pb-3 shadow-md'>
+        <div class='col-span-1 grid grid-rows-6'>
+            <div class='row-span-5'>
+                <img
+                    src="{{ $this->candidate->profile_photo_url }}"
+                    alt="{{ $this->candidate->name }}"
+                    class="h-36 w-36 border border-black"
+                    loading='lazy'>
+            </div>
+            <div class='row-span-1 text-center'>
+                <h1 class='text-xl font-semibold'>{{ $this->candidate->name }}</h1>
+            </div>
+        </div>
+
+        {{-- TODO: Mobile should have a show more --}}
+        @if($this->candidate->bio != "")
+            <div class='col-span-2'>
+                <p class='max-w-prose text-base'>
+                    {{$this->candidate->bio}}
+                </p>
+            </div>
+        @endif
+</article>
+
+
