52ea7dead49f48c25c57247a21124009e6485a7e
pjared <pjared870@gmail.com>
2022-09-08 11:46:35 -0600

    Changes text color of descriptive info

--- resources/views/components/show-more.blade.php
+++ resources/views/components/show-more.blade.php
@@ -1,8 +1,9 @@
 <p
     x-data="{ isCollapsed: false, maxLength: 215, originalContent: '', content: '' }"
     x-init="originalContent = $el.firstElementChild.textContent.trim(); content = originalContent.slice(0, maxLength)"
+
     >
-    <span x-text="isCollapsed ? originalContent : content">
+    <span x-text="isCollapsed ? originalContent : content" class='text-gray-700'>
         {{$content}}
     </span>
     <span x-show="originalContent.length > maxLength && !isCollapsed">
