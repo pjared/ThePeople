a23646d06248e24802ba7e6c4e515a938355a9a2
pjared <pjared870@gmail.com>
2022-09-08 11:43:05 -0600

    Updates UI for promises component

--- resources/views/candidate/component/promises.blade.php
+++ resources/views/candidate/component/promises.blade.php
@@ -1,19 +1,19 @@
 {{-- Promises --}}
 @if($this->candidate->promises_count >= 1)
-    <article class="flex grow flex-col w-11/12 justify-center items-center" x-data="{open: false}">
-        <x-dropdown-card>
+    <article class="flex flex-col w-11/12 justify-center items-center">
+        <x-dropdown-card :classes="'shadow-xl'">
             <x-slot:title>
                 Candidate's Promises
             </x-slot>
             <x-slot:content>
-                <div class="flex flex-col w-full gap-2">
-                    <p class="text-center">
-                        This candidate promises to make the reforms or stand for the values listed below.
-                    </p>
+                <p class="text-center text-gray-400">
+                    This candidate promises to make the reforms or stand for the values listed below.
+                </p>
+                <div class="flex flex-col w-full gap-6 pt-4">
                     @foreach($promises as $promise)
-                        <div class="flex flex-row justify-center items-center gap-4">
-                            <div class="flex flex-col items-center">
-                                <h3 class="w-fit"><b>{{ $promise->promise }}</b></h3>
+                        <div class="flex flex-row justify-center items-start">
+                            <div class="flex flex-col w-full text-start">
+                                <h3 class="w-fit capitalize"><b>{{ $promise->promise }}</b></h3>
                                 <x-show-more :content="$promise->plan" />
                             </div>
                             @auth
