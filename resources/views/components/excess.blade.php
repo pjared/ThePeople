fb4bae586b8194c55c2683e35adc5ac487666aae
pjared <pjared870@gmail.com>
2022-09-08 11:46:23 -0600

    Adds a file for future/removed components

---
+++ resources/views/components/excess.blade.php
@@ -0,0 +1,8 @@
+{{-- A blade full of components that I'm not sure if I'd like to use --}}
+
+{{--    Circular Profile Image
+    <img
+        src="{{ $this->candidate->profile_photo_url ?? ''}}"
+        alt="{{ $this->candidate->name }}"
+        class="h-36 w-36 mask mask-circle border border-black"
+        loading='lazy'> --}}
