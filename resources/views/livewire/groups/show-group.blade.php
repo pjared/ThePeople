cfb06e74ccc4af18e091506f09b18d95463953bd
pjared <pjared870@gmail.com>
2022-09-09 15:01:40 -0600

    Basic update for group page

--- resources/views/livewire/groups/show-group.blade.php
+++ resources/views/livewire/groups/show-group.blade.php
@@ -1,23 +1,39 @@
-<div class='grid grid-rows-6 gap-4 w-full h-full'>
-    <div class='row-span-2 flex w-full relative text-center'>
-        {{-- Image --}}
-        <span
-            class='absolute antialiased text-white text-2xl font-bold'
-            style="top: 50%; left: 50%; transform: translate(-50%, -50%);">
-                {{$group->name}}
-        </span>
-        <img
-            class="w-full h-80"
-            src="https://placeimg.com/400/300/arch"
-            loading='lazy'/>
+<div class='flex flex-col gap-4 w-full items-center justify-center h-full'>
+    <div class='flex flex-col justify-center items-center w-full bg-white'>
+        <div class='flex w-3/5 relative justify-center'>
+            {{-- Image --}}
+            <img
+                class="w-full h-80"
+                src="https://placeimg.com/400/300/arch"
+                loading='lazy'/>
+        </div>
+        <div class='flex flex-row items-center justify-start pl-8 w-3/5'>
+            <div class='relative w-32 h-20 items-center'>
+                <img
+                    class="absolute bottom-1 rounded-full border-4 border-white w-28 h-28 mask mask-circle"
+                    src="https://placeimg.com/400/300/arch"
+                    loading='lazy'/>
+            </div>
+            <h1
+                class='antialiased bold text-2xl font-bold'>
+                    {{$group->name}}
+            </h1>
+        </div>
+
     </div>
-    <div class='row-span-4 flex flex-row px-12'>
+
+    <div class='flex flex-row w-11/12 px-12'>
         <div class='w-4/5'>
             <div class='flex flex-col w-11/12 background-card items-center'>
+                {{-- ABOUT US --}}
+                <div class='flex flex-col text-center'>
+                    <h2 class='text-xl font-semibold'>About Us</h2>
+                    <p class='text-gray-700'>{{$group->description}}</p>
+                </div>
                 {{-- EVENTS --}}
                 @if(count($group->events) >= 1)
                     <div class='flex flex-col w-full items-center'>
-                        <span>Upcoming Events</span>
+                        <h2 class='text-lg font-semibold'>Upcoming Events</h2>
                         <div class='flex w-full justify-center'>
                             @foreach($group->events as $event)
                                 <div class="flex flex-col gap-4 w-1/2">
@@ -31,11 +47,7 @@
                         </div>
                     </div>
                 @endif
-                {{-- ABOUT US --}}
-                <div class='flex flex-col text-center'>
-                    <span>About Us</span>
-                    <span>{{$group->description}}</span>
-                </div>
+
             </div>
 
         </div>
@@ -46,4 +58,11 @@
             </div>
         </div>
     </div>
+
+    @section('page-title')
+        {{$this->group->name}}
+    @endsection
+    @section('description')
+        The profile for the political group {{$this->group->name}}. On a group's profile you can find information about the group, their upcoming events, along with their recommended voting list.
+    @endsection
 </div>
