a8c12cd631da234e6b3908cced2fb855e31a8125
pjared <pjared870@gmail.com>
2022-09-09 11:57:24 -0600

    Adds shadowing for political group list

--- resources/views/livewire/groups/political-group-list.blade.php
+++ resources/views/livewire/groups/political-group-list.blade.php
@@ -2,7 +2,7 @@
     <div class="flex flex-col md:flex-row flex-wrap gap-8 w-3/4 items-center justify-center">
         @foreach ($groups as $group)
             <form action="/groups/{{$group->slug}}" method="GET" class="hover:scale-110" x-data="{ show: false }" @mouseleave="show = false" @mouseover="show = true" >
-                <button class="background-card" class="card bg-base-100 shadow-xl">
+                <button class="background-card shadow-md" class="card bg-base-100 shadow-xl">
                     <div class="card-body text-center p-0">
                         <h2 class="card-title text-lg justify-center">
                             <a
