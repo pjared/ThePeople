92019ef7d662dfca8a5815620c1a7cbe79fb4ce8
pjared <pjared870@gmail.com>
2022-09-12 11:33:15 -0600

    Updates UI for ballot list

--- resources/views/livewire/ballot/ballot-list.blade.php
+++ resources/views/livewire/ballot/ballot-list.blade.php
@@ -5,14 +5,14 @@
                 <form action="/ballot/{{$ballot->slug}}" method="GET" class="hover:scale-110" x-data="{ show: false }" @mouseleave="show = false" @mouseover="show = true" >
                     <button class="background-card shadow-md" class="card bg-base-100 shadow-xl">
                         <div class="text-center p-0">
-                            <h2 class="card-title text-lg justify-center">
+                            <h2 class="card-title text-xl justify-center">
                                 <a
                                     rel="next prefetch canonical"
-                                    class="underline text-sky-600"
+                                    class="underline text-sky-600 font-mono tracking-tighter font-light"
                                     type="text/html"
                                     href="/ballot/{{$ballot->slug}}">{{$ballot->location->state}} {{$ballot->office->name}}, {{$ballot->location->name}}</a>
                             </h2>
-                            <div x-show="show" class="uppercase mt-2 text-sm justify-center">
+                            <div x-show="show" class="uppercase mt-2 text-sm font-mono text-gray-400 justify-center">
                                 <p>
                                     VOTING DATE: {{$ballot->voting_date->format('m/d/Y')}}
                                 </p>
