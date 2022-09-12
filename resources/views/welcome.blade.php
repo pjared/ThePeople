b1093c6eb5ffbc75c3f63f98befc92127edd6864
pjared <pjared870@gmail.com>
2022-09-12 11:16:54 -0600

    Improves website header

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <div class="grid grid-flow-row bg-gray-50 gap-4">
         <div class="mt-16 h-12 flex col-span-1 justify-center">
-            <div class="text-xl font-roboto_mono md:text-4xl type-out">
+            <div class="text-xl font-roboto_mono tracking-tight md:text-4xl type-out">
                 <h1>See Who's Running For ...</h1>
             </div>
         </div>
