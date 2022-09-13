8091522ec679e6342cfb4dc79a234c92cc2b3496
pjared <pjared870@gmail.com>
2022-09-13 11:34:36 -0600

    Adds ability to search on ballot list

--- app/Http/Livewire/Ballot/BallotList.php
+++ app/Http/Livewire/Ballot/BallotList.php
@@ -3,27 +3,29 @@
 namespace App\Http\Livewire\Ballot;
 
 use App\Models\Ballot;
+use App\Models\Candidate;
 use Illuminate\Support\Facades\Cache;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class BallotList extends Component
 {
-    // public $ballots = [];
+    use WithPagination;
+    public $search = '';
 
     public function render()
     {
-        return view('livewire.ballot.ballot-list');
+        usleep(500);
+        return view('livewire.ballot.ballot-list', [
+            'candidate_searches' => empty($this->search) ? [] : Candidate::search($this->search)->paginate(5),
+            'ballot_searches' => empty($this->search) ? [] : Ballot::search($this->search)->paginate(5),
+        ]);
     }
 
     public function getBallotsProperty()
     {
-        return $this->ballots = Cache::rememberForever('ballots', function () {
+        return Cache::rememberForever('ballots', function () {
             return Ballot::with('office', 'location')->withCount('candidates')->get();
         });
-    }
-
-    public function load_ballots()
-    {
-        $this->ballots = $this->getBallotsProperty();
     }
 }
--- resources/views/livewire/ballot/ballot-list.blade.php
+++ resources/views/livewire/ballot/ballot-list.blade.php
@@ -1,5 +1,43 @@
-<div class="flex grow justify-center mt-12 p-6">
-    <div class="flex flex-col md:flex-row flex-wrap gap-8 w-3/4 items-center justify-center" wire:init="load_ballots">
+<div class="flex flex-col grow justify-center items-center gap-10">
+    <div class="pt-2 relative mx-auto text-gray-600 w-2/5">
+        <input class="border-2 border-gray-300 bg-white h-12 px-5 pr-16 w-full rounded-lg text-sm focus:outline-none"
+            type="search" wire:model='search'  name="search" placeholder="Search for a Candidate, Public Office, or Location">
+        <button class="absolute right-0 top-1 mt-5 mr-4">
+            <svg class="text-gray-600 h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px"
+            viewBox="0 0 56.966 56.966" style="enable-background:new 0 0 56.966 56.966;" xml:space="preserve"
+            width="512px" height="512px">
+            <path
+                d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
+            </svg>
+        </button>
+
+        <ul class='absolute z-10 w-full bg-white rounded-t-none rounded-md shadow-lg list-group' id="myUL">
+            @foreach ($candidate_searches as $result)
+                <li class='list-item border-b-2 border-gray-300'>
+                    <form class='p-2' action="/candidate/profile/{{$result->slug}}" method="GET">
+                        {{-- grid grid-flow-col grid-cols-4 span-cols-1 --}}
+                        <div class='flex flex-col pl-2'>
+                            <a class='link underline'>{{$result->name}}</a>
+                            <p class='text-gray-400'>{{$result->state}}</p>
+                        </div>
+                    </form>
+                </li>
+            @endforeach
+            @foreach ($ballot_searches as $result)
+                <li class='list-item border-b-2 border-gray-300 p-2'>
+                    <form class='p-2' action="/ballot/{{$result->slug}}" method="GET">
+                        <a class='link underline'>{{$result->name}}</a>
+                    </form>
+                </li>
+            @endforeach
+            @if(count($candidate_searches) == 0 && count($ballot_searches) == 0 && !empty($search))
+                <p class='p-2'>We couldn't find any results for your search. For the upcoming elections on November 8th, 2022 we are focusing pimarily on the elections for the Provo/Orem area. If you'd like to see your upcoming election appear here, please contact us at thepeople@whatsinyourballot.com and we'll see what we can do!</p>
+            @endif
+        </ul>
+    </div>
+
+    <div class="flex flex-col md:flex-row flex-wrap gap-8 w-3/4 items-center justify-center">
         @foreach ($this->ballots as $ballot)
             @if($ballot->candidates_count >= 1)
                 <form action="/ballot/{{$ballot->slug}}" method="GET" class="hover:scale-110" x-data="{ show: false }" @mouseleave="show = false" @mouseover="show = true" >
