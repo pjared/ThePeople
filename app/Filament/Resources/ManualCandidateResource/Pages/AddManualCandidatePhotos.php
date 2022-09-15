4bb07a1cf7de2ff187d28162bc9d897c53365b95
pjared <pjared870@gmail.com>
2022-09-15 16:51:55 -0600

    Adds ability to add candidate photo

--- app/Filament/Resources/ManualCandidateResource/Pages/AddManualCandidatePhotos.php
+++ app/Filament/Resources/ManualCandidateResource/Pages/AddManualCandidatePhotos.php
@@ -4,12 +4,16 @@ namespace App\Filament\Resources\ManualCandidateResource\Pages;
 
 use Filament\Tables;
 use App\Filament\Resources\ManualCandidateResource;
+use App\Jobs\UpdateBallotCache;
+use App\Models\Candidate;
 use App\Models\CandidatePhoto;
 use App\Models\ManualCandidate;
 use Filament\Resources\Pages\Page;
 use Filament\Pages\Actions\Action;
 use Illuminate\Database\Eloquent\Builder;
 use Filament\Forms;
+use Filament\Notifications\Notification;
+use Spatie\ImageOptimizer\OptimizerChainFactory;
 
 class AddManualCandidatePhotos extends Page implements Tables\Contracts\HasTable
 {
@@ -17,15 +21,17 @@ class AddManualCandidatePhotos extends Page implements Tables\Contracts\HasTable
     protected static string $resource = ManualCandidateResource::class;
 
     protected static string $view = 'filament.resources.manual-candidate-resource.pages.add-manual-candidate-photos';
-    public $candidate_id;
+    public $photo;
+    public $candidate;
+
     public function mount(ManualCandidate $record) {
-        $this->candidate_id = $record->candidate_id;
+        $this->candidate = Candidate::find($record->candidate_id);
     }
 
 
     protected function getTableQuery(): Builder
     {
-        return CandidatePhoto::query()->where('candidate_id', $this->candidate_id);
+        return CandidatePhoto::query()->where('candidate_id', $this->candidate->id);
     }
 
     protected function getTableColumns(): array
@@ -49,7 +55,7 @@ class AddManualCandidatePhotos extends Page implements Tables\Contracts\HasTable
         return [
             Action::make('Add Stance')->action(function (array $data): void {
                 CandidatePhoto::create([
-                    'candidate_id' => $this->candidate_id,
+                    'candidate_id' => $this->candidate->id,
                     'attachment' => $data['attachment'],
                     'order' => $data['order'],
                 ]);
@@ -73,4 +79,39 @@ class AddManualCandidatePhotos extends Page implements Tables\Contracts\HasTable
         ];
     }
 
+    /**
+     * Candidates are going to upload a photo.
+     * The catch is, they don't know how to even use a computer. I would be surprised if a candidate
+     * could tell me what image metadata is. So we're going to strip that for them.
+     */
+    public function update_photo()
+    {
+        // 2 Levels of validation. Make sure that it's an image first
+        $this->validate([
+            'photo' => 'image',
+        ]);
+        //Now lets run it through and optimizer
+        $optimizerChain = OptimizerChainFactory::create();
+        $optimizerChain->optimize($this->photo->getRealPath());
+
+        // Now lets make sure that it's less than a mb
+        $this->validate([
+            'photo' => 'max:1024'
+        ]);
+        if (isset($this->photo)) {
+            $this->candidate->updateProfilePhoto($this->photo);
+            // auth()->user()->updateProfilePhoto($this->photo);
+        }
+        Notification::make()
+        ->title('Saved successfully')
+        ->success()
+        ->send();
+
+        UpdateBallotCache::dispatch($this->candidate->ballot);
+    }
+
+    public function deleteProfilePhoto() {
+        // auth()->user()->deleteProfilePhoto($this->photo);
+        $this->candidate->updateProfilePhoto($this->photo);
+    }
 }
--- resources/views/filament/resources/manual-candidate-resource/pages/add-manual-candidate-photos.blade.php
+++ resources/views/filament/resources/manual-candidate-resource/pages/add-manual-candidate-photos.blade.php
@@ -1,4 +1,61 @@
 <x-filament::page>
+    <x-filament::card>
+        <div class="flex flex-col p-4 gap-6 items-center">
+            <div class="card w-96 bg-base-100 shadow-xl">
+                <div class="card-body bg-white">
+                    <form wire:submit.prevent="update_photo">
+                        <div x-data="{photoName: null, photoPreview: null}" class="col-span-6 sm:col-span-4">
+                            <!-- Profile Photo File Input -->
+                            <input type="file" class="hidden"
+                                        wire:model="photo"
+                                        x-ref="photo"
+                                        x-on:change="
+                                                photoName = $refs.photo.files[0].name;
+                                                const reader = new FileReader();
+                                                reader.onload = (e) => {
+                                                    photoPreview = e.target.result;
+                                                };
+                                                reader.readAsDataURL($refs.photo.files[0]);
+                                        " />
+                            <x-jet-label for="photo" value="{{ __('Photo') }}" />
+
+                            <!-- Current Profile Photo -->
+                            <div class="mt-2" x-show="! photoPreview">
+                                {{-- <img src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" class="rounded-full h-20 w-20 object-cover"> --}}
+                                <img
+                                    src="{{ $candidate->profile_photo_url }}"
+                                    alt="{{ $candidate->name }}"
+                                    class="rounded-full h-20 w-20 object-cover">
+                            </div>
+
+                            <!-- New Profile Photo Preview -->
+                            <div class="mt-2" x-show="photoPreview" style="display: none;">
+                                <span class="block rounded-full w-20 h-20 bg-cover bg-no-repeat bg-center"
+                                    x-bind:style="'background-image: url(\'' + photoPreview + '\');'">
+                                </span>
+                            </div>
+
+                            <x-jet-secondary-button class="mt-2 mr-2" type="button" x-on:click.prevent="$refs.photo.click()">
+                                {{ __('Select A New Photo') }}
+                            </x-jet-secondary-button>
+
+                            @if (Auth::user()->profile_photo_path)
+                                <x-jet-secondary-button type="button" class="mt-2" wire:click="deleteProfilePhoto">
+                                    {{ __('Remove Photo') }}
+                                </x-jet-secondary-button>
+                            @endif
+
+                            <x-jet-input-error for="photo" class="mt-2" />
+                        </div>
+                        <div class="card-actions justify-end mt-2">
+                            <button class='btn' type='submit'>Save</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </x-filament::card>
+
     <div>
         {{ $this->table }}
     </div>
