513b73d5f15b86fdbd8e047e41292d7f4ef5ba7e
pjared <pjared870@gmail.com>
2022-09-20 15:48:16 -0600

    Updates modal message

--- resources/views/candidate/preview.blade.php
+++ resources/views/candidate/preview.blade.php
@@ -3,6 +3,6 @@
     <div class="flex flex-row">
         @include('candidate.aside')
         <livewire:candidate.profile :candidate="$candidate">
-    </div>    
-    @include('modals.signup', ['message' => "Please log in or register if you'd like to save information about the candidate. A candiate comparison page will be implemented soon!"])    
+    </div>
+    @include('modals.signup', ['message' => "Please log in or register if you'd like to save information about the candidate."])
 </x-app-layout>
