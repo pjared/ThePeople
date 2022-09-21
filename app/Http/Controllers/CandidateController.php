eb30076e5d051180fb01571f8da7deb1a792475a
pjared <pjared870@gmail.com>
2022-09-20 19:09:36 -0600

    Updates candidate controller

--- app/Http/Controllers/CandidateController.php
+++ app/Http/Controllers/CandidateController.php
@@ -3,9 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\Candidate;
-use App\Models\ControversialOpinion;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 
 class CandidateController extends Controller
@@ -13,14 +10,14 @@ class CandidateController extends Controller
     /**
      * Find the candidate in the database, and populate candidate page
      */
-    public function getView($candidate_slug) {
+    public function getView(Candidate $candidate) {
         if(! auth()->check()) {
-            if(Storage::disk('export')->exists('/candidate/profile/' . $candidate_slug . '/index.html')) {
-                return Storage::disk('export')->get('/candidate/profile/' . $candidate_slug . '/index.html');
+            if(Storage::disk('export')->exists('/candidate/profile/' . $candidate->slug . '/index.html')) {
+                return Storage::disk('export')->get('/candidate/profile/' . $candidate->slug . '/index.html');
             }
         }
 
-        $candidate = Candidate::where('slug', $candidate_slug)->select('id', 'name' , 'slug')->first();
+        $candidate = Candidate::firstWhere('slug', $candidate->slug);
         $candidate->load('ballot', 'ballot.office:id,name', 'ballot.location:id,state,name');
         return view('candidate.show')
                     ->with('candidate', $candidate);
