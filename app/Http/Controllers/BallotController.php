cb4cf1878abdb463176d35eaac55a5017b389a36
pjared <pjared870@gmail.com>
2022-09-23 11:11:16 -0600

    Reduces queries for static page

--- app/Http/Controllers/BallotController.php
+++ app/Http/Controllers/BallotController.php
@@ -8,14 +8,15 @@ use Illuminate\Support\Facades\Storage;
 
 class BallotController extends Controller
 {
-    public function getView(Ballot $ballot)
+    public function getView($ballot_slug)
     {
         if(! auth()->check()) {
-            if(Storage::disk('export')->exists('ballot/' . $ballot->slug . '/index.html')) {
-                return Storage::disk('export')->get('ballot/' . $ballot->slug . '/index.html');
+            if(Storage::disk('export')->exists('ballot/' . $ballot_slug . '/index.html')) {
+                return Storage::disk('export')->get('ballot/' . $ballot_slug . '/index.html');
             }
         }
-        $ballot = Cache::rememberForever('ballot-' . $ballot->slug, function () use ($ballot) {
+        $ballot = Cache::rememberForever('ballot-' . $ballot_slug, function () use ($ballot_slug) {
+            $ballot = Ballot::firstWhere('slug', $ballot_slug);
             return $ballot->load('location:id,name,state',
                                     'office:id,name',
                                     'candidates');
--- app/Http/Controllers/CandidateController.php
+++ app/Http/Controllers/CandidateController.php
@@ -10,14 +10,14 @@ class CandidateController extends Controller
     /**
      * Find the candidate in the database, and populate candidate page
      */
-    public function getView(Candidate $candidate) {
+    public function getView($candidate_slug) {
         if(! auth()->check()) {
-            if(Storage::disk('export')->exists('/candidate/profile/' . $candidate->slug . '/index.html')) {
-                return Storage::disk('export')->get('/candidate/profile/' . $candidate->slug . '/index.html');
+            if(Storage::disk('export')->exists('/candidate/profile/' . $candidate_slug . '/index.html')) {
+                return Storage::disk('export')->get('/candidate/profile/' . $candidate_slug . '/index.html');
             }
         }
 
-        $candidate = Candidate::firstWhere('slug', $candidate->slug);
+        $candidate = Candidate::firstWhere('slug', $candidate_slug);
         $candidate->load('ballot', 'ballot.office:id,name', 'ballot.location:id,state,name');
         return view('candidate.show')
                     ->with('candidate', $candidate);
