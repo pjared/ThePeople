bf55a66178eb28924beb010057cd72d56a67831c
pjared <pjared870@gmail.com>
2022-09-27 12:56:35 -0600

    Only shows component if count

--- resources/views/candidate/component/background.blade.php
+++ resources/views/candidate/component/background.blade.php
@@ -1,39 +1,37 @@
 {{-- PREVIOUS POSITIONS --}}
-<article class="flex grow flex-col w-11/12 items-center">
-    <x-dropdown-card :classes="'shadow-md'">
-        <x-slot:title>
-            Background Experience
-        </x-slot>
-        <x-slot:content>
-            @if($this->candidate->backgrounds_count >= 1)
-                <div class="flex flex-col gap-4">
-                    @foreach($this->candidate->backgrounds as $background)
-                        <div class="flex flex-row justify-center items-center gap-6">
-                            <div class="flex flex-col w-full items-start">
-                                <h3 class="w-fit capitalize text-lg font-semibold">
-                                    {{ $background->name }}
-                                    <span class='text-sm text-gray-400'>{{ $background->year_start }} - {{ $background->year_end }}</span>
-                                </h3>
-                                <x-show-more :content="$background->description" />
+@if($this->candidate->backgrounds_count >= 1)
+    <article class="flex grow flex-col w-11/12 items-center">
+        <x-dropdown-card :classes="'shadow-md'">
+            <x-slot:title>
+                Background Experience
+            </x-slot>
+            <x-slot:content>
+                    <div class="flex flex-col gap-4">
+                        @foreach($this->candidate->backgrounds as $background)
+                            <div class="flex flex-row justify-center items-center gap-6">
+                                <div class="flex flex-col w-full items-start">
+                                    <h3 class="w-fit capitalize text-lg font-semibold">
+                                        {{ $background->name }}
+                                        <span class='text-sm text-gray-400'>{{ $background->year_start }} - {{ $background->year_end }}</span>
+                                    </h3>
+                                    <x-show-more :content="$background->description" />
+                                </div>
+                                @auth
+                                    {{-- {{dd($flags->where('flaggable_type', 'App\Models\CandidateOfficePositions')->where('flaggable_id', 2), $previous_positions)}} --}}
+                                    <livewire:flag-content
+                                        :flag="$this->flags->where('flaggable_type', 'App\Models\CandidateBackground')->firstWhere('flaggable_id', $background->id)"
+                                        :content="$background"
+                                        :side="'below'"
+                                        :wire:key="'background-'.$background->id" />
+                                @else
+                                    <label class="fill-transparent" for="signup-modal">
+                                        @include('icons.flag')
+                                    </label>
+                                @endauth
                             </div>
-                            @auth
-                                {{-- {{dd($flags->where('flaggable_type', 'App\Models\CandidateOfficePositions')->where('flaggable_id', 2), $previous_positions)}} --}}
-                                <livewire:flag-content
-                                    :flag="$this->flags->where('flaggable_type', 'App\Models\CandidateBackground')->firstWhere('flaggable_id', $background->id)"
-                                    :content="$background"
-                                    :side="'below'"
-                                    :wire:key="'background-'.$background->id" />
-                            @else
-                                <label class="fill-transparent" for="signup-modal">
-                                    @include('icons.flag')
-                                </label>
-                            @endauth
-                        </div>
-                    @endforeach
-                </div>
-            @else
-                This Candidate has not held office before
-            @endif
-        </x-slot>
-    </x-dropdown-card>
-</article>
+                        @endforeach
+                    </div>
+            </x-slot>
+        </x-dropdown-card>
+    </article>
+@endif
--- resources/views/candidate/component/education.blade.php
+++ resources/views/candidate/component/education.blade.php
@@ -1,38 +1,38 @@
 {{-- PREVIOUS EDUCATION --}}
-<article class="flex grow flex-col w-11/12 items-center">
-    <x-dropdown-card :classes="'shadow-md'">
-        <x-slot:title>
-            Education
-        </x-slot>
-        <x-slot:content>
-            @if($this->candidate->educations_count >= 1)
-                <div class="flex flex-col gap-4">
-                    @foreach($this->candidate->educations as $education)
-                        <div class="flex flex-row justify-center items-center gap-6">
-                            <div class="flex flex-col w-full items-start">
-                                <h3 class="w-fit capitalize text-lg font-semibold">
-                                    {{ $education->school_name }}
-                                    <span class='text-sm text-gray-400'>{{ $education->year_start }} - {{ $education->year_end }}</span>
-                                </h3>
-                                <div><b>{{$education->degree_type}}</b> in {{$education->degree_field}}</div>
+@if($this->candidate->educations_count >= 1)
+    <article class="flex grow flex-col w-11/12 items-center">
+        <x-dropdown-card :classes="'shadow-md'">
+            <x-slot:title>
+                Education
+            </x-slot>
+            <x-slot:content>
+
+                    <div class="flex flex-col gap-4">
+                        @foreach($this->candidate->educations as $education)
+                            <div class="flex flex-row justify-center items-center gap-6">
+                                <div class="flex flex-col w-full items-start">
+                                    <h3 class="w-fit capitalize text-lg font-semibold">
+                                        {{ $education->school_name }}
+                                        <span class='text-sm text-gray-400'>{{ $education->year_start }} - {{ $education->year_end }}</span>
+                                    </h3>
+                                    <div><b>{{$education->degree_type}}</b> in {{$education->degree_field}}</div>
+                                </div>
+                                @auth
+                                    <livewire:flag-content
+                                        :flag="$this->flags->where('flaggable_type', 'App\Models\CandidateEducation')->firstWhere('flaggable_id', $education->id)"
+                                        :content="$education"
+                                        :side="'below'"
+                                        :wire:key="'education-'.$education->id" />
+                                @else
+                                    <label class="fill-transparent" for="signup-modal">
+                                        @include('icons.flag')
+                                    </label>
+                                @endauth
                             </div>
-                            @auth
-                                <livewire:flag-content
-                                    :flag="$this->flags->where('flaggable_type', 'App\Models\CandidateEducation')->firstWhere('flaggable_id', $education->id)"
-                                    :content="$education"
-                                    :side="'below'"
-                                    :wire:key="'education-'.$education->id" />
-                            @else
-                                <label class="fill-transparent" for="signup-modal">
-                                    @include('icons.flag')
-                                </label>
-                            @endauth
-                        </div>
-                    @endforeach
-                </div>
-            @else
-                This Candidate has not held office before
-            @endif
-        </x-slot>
-    </x-dropdown-card>
-</article>
+                        @endforeach
+                    </div>
+
+            </x-slot>
+        </x-dropdown-card>
+    </article>
+@endif
