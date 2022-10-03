c138c0d8ec6fa065c6e145bb5d6923d8889edd12
pjared <pjared870@gmail.com>
2022-10-03 13:56:08 -0600

    Gets the cached candidate in controller

--- app/Http/Controllers/CandidateController.php
+++ app/Http/Controllers/CandidateController.php
@@ -3,11 +3,23 @@
 namespace App\Http\Controllers;
 
 use App\Models\Candidate;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Storage;
 
 class CandidateController extends Controller
 {
+    public function getCandidate($slug) {
+        return Cache::remember('candidate-' . $slug, 120, function () use ($slug) {
+            $candidate = Candidate::firstWhere('slug', $slug);
+            return $candidate->load('ballot', 'ballot.office:id,name', 'ballot.location:id,state,name',
+                                'required_stances', 'stances',
+                                'opinions', 'manual_candidate', 'educations', 'backgrounds')
+                                ->loadCount('events', 'required_stances', 'stances', 'promises', 'videos', 'previous_positions',
+                                'opinions', 'educations', 'backgrounds');
+        });
+        // 'events', 'videos', 'promises', 'previous_positions',
+    }
     /**
      * Find the candidate in the database, and populate candidate page
      */
@@ -18,9 +30,22 @@ class CandidateController extends Controller
             }
         }
 
-        $candidate = Candidate::firstWhere('slug', $candidate_slug);
-        $candidate->load('ballot', 'ballot.office:id,name', 'ballot.location:id,state,name');
+        $candidate = $this->getCandidate($candidate_slug);
+        $is_manual = is_null($candidate->user_id);
+        if ($candidate->ballot) {
+            $opinions = $candidate->ballot->opinions->load(['required_stances']);
+        } else {
+            $opinions = [];
+        }
+        if(! auth()->check()) {
+            $flags = [];
+        } else {
+            $flags =  auth()->user()->flags->where('ballot_id', $candidate->ballot->id)->where('candidate_id', $candidate->id);
+        }
         return view('candidate.show')
-                    ->with('candidate', $candidate);
+                    ->with('candidate', $candidate)
+                    ->with('opinions', $opinions)
+                    ->with('flags', $flags)
+                    ->with('is_manual', $is_manual);
     }
 }
