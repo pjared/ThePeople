0c08e58c5ec4f26eb5b433fd9a6fe034008aba3d
pjared <pjared870@gmail.com>
2022-10-17 14:05:13 -0600

    Updates candidate donors model

--- app/Models/CandidateDonors.php
+++ app/Models/CandidateDonors.php
@@ -3,18 +3,32 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\Pivot;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class CandidateDonors extends Pivot
+class CandidateDonors extends Model
 {
     use HasFactory;
-    use SoftDeletes;
 
     public $timestamps = false;
 
+    protected $table = 'candidate_donor';
+
     protected $guarded = [
         'candidate_id',
-        'donor_id',
+        'top_5_donors',
+        'top_5_states',
+        'top_5_groups',
+        'total',
     ];
+
+    protected $casts = [
+        'top_5_donors' => 'array',
+        'top_5_states' => 'array',
+        'top_5_groups' => 'array',
+    ];
+
+    public function candidate() {
+        return $this->belongsTo(Candidate::class, 'candidate_id');
+    }
 }
---
+++ database/migrations/2022_10_17_192919_update_candidate_donor_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('candidate_donor', function (Blueprint $table) {
+            $table->dropColumn('donor_id');
+            $table->decimal('total');
+            $table->text('top_5_groups');
+            $table->text('top_5_states');
+            $table->text('top_5_donors');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('candidate_donor', function (Blueprint $table) {
+            $table->foreignId('donor_id');
+            $table->dropColumn('total');
+            $table->dropColumn('top_5_groups');
+            $table->dropColumn('top_5_states');
+            $table->dropColumn('top_5_donors');
+        });
+    }
+};
